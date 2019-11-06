Fixed a bug in Neuron_morphology such that if a neuron was created with a rotation matrix passed, and loaded from cache, the rotation happened twice

Neuron_morphology.py
@@ -110,7 +110,7 @@ class NeuronMorphology(object):
       if(self.cacheExist()):
         # Load existing cache
         try:
-          self.loadCache()
+          self.loadCache() 
         except Exception as e:
           
           import traceback
@@ -266,6 +266,7 @@ class NeuronMorphology(object):
   # We can specify a position and rotation
   def place(self, rotation=None, position=None):
 
+    
     if(self.rotatedFlag):
       self.writeLog("!!! WARNING, rotating a rotated neuron...")
     
@@ -278,6 +279,9 @@ class NeuronMorphology(object):
       position = self.position
     elif(type(position) is not np.ndarray):
       position = np.array(position)
+
+    print("Place called! pos = " + str(position) + ", rot = " + str(rotation))
+
       
     # rotation = self.randRotationMatrix()
       
@@ -428,8 +432,8 @@ class NeuronMorphology(object):
     else:
       self.axonDensity = None
       
-    # Place neuron
-    self.place()
+    # Place neuron -- Do not place neuron, loadNeuronMorphology does that
+    # self.place()
 
       
   ############################################################################
@@ -742,7 +746,7 @@ class NeuronMorphology(object):
       
   ############################################################################
 
-  def plotNeuron(self,axis=None,plotAxon=True,plotDendrite=True,lineStyle='-',alpha=1.0):
+  def plotNeuron(self,axis=None,plotAxon=True,plotDendrite=True,lineStyle='-',alpha=1.0,plotOrigo=np.array([0,0,0]),plotScale=1.0):
     
     if(self.verbose):
       print("Plotting neuron " + self.swc_filename)
@@ -767,9 +771,9 @@ class NeuronMorphology(object):
         elif(row[1] == axLinks[-1]):
           axLinks.append(row[0])
         else:
-          ax.plot(self.axon[axLinks,0],
-                  self.axon[axLinks,1],
-                  self.axon[axLinks,2],
+          ax.plot((self.axon[axLinks,0]-plotOrigo[0])*plotScale,
+                  (self.axon[axLinks,1]-plotOrigo[1])*plotScale,
+                  (self.axon[axLinks,2]-plotOrigo[2])*plotScale,
                   linestyle=lineStyle,
                   marker=',',
                   alpha=alpha,
@@ -778,9 +782,9 @@ class NeuronMorphology(object):
           axLinks = list(row)
 
       if(len(axLinks) > 0):
-          ax.plot(self.axon[axLinks,0],
-                  self.axon[axLinks,1],
-                  self.axon[axLinks,2],
+          ax.plot((self.axon[axLinks,0]-plotOrigo[0])*plotScale,
+                  (self.axon[axLinks,1]-plotOrigo[1])*plotScale,
+                  (self.axon[axLinks,2]-plotOrigo[2])*plotScale,
                   linestyle=lineStyle,
                   marker=',',
                   alpha=alpha,
@@ -796,9 +800,9 @@ class NeuronMorphology(object):
         elif(row[1] == dendLinks[-1]):
           dendLinks.append(row[0])
         else:
-          ax.plot(self.dend[dendLinks,0],
-                  self.dend[dendLinks,1],
-                  self.dend[dendLinks,2],
+          ax.plot((self.dend[dendLinks,0]-plotOrigo[0])*plotScale,
+                  (self.dend[dendLinks,1]-plotOrigo[1])*plotScale,
+                  (self.dend[dendLinks,2]-plotOrigo[2])*plotScale,
                   linestyle=lineStyle,
                   marker=',',
                   alpha=alpha,
@@ -807,9 +811,9 @@ class NeuronMorphology(object):
           dendLinks = list(row)
 
       if(len(dendLinks) > 0):
-        ax.plot(self.dend[dendLinks,0],
-                self.dend[dendLinks,1],
-                self.dend[dendLinks,2],
+        ax.plot((self.dend[dendLinks,0]-plotOrigo[0])*plotScale,
+                (self.dend[dendLinks,1]-plotOrigo[1])*plotScale,
+                (self.dend[dendLinks,2]-plotOrigo[2])*plotScale,
                 linestyle=lineStyle,
                 marker=',',
                 alpha=alpha,
@@ -817,7 +821,10 @@ class NeuronMorphology(object):
         
           
     if(len(self.soma) > 0):
-      ax.scatter(self.soma[:,0],self.soma[:,1],self.soma[:,2],c=self.colour,alpha=alpha)
+      ax.scatter((self.soma[:,0]-plotOrigo[0])*plotScale,
+                 (self.soma[:,1]-plotOrigo[1])*plotScale,
+                 (self.soma[:,2]-plotOrigo[2])*plotScale,
+                 c=self.colour,alpha=alpha)
       
     plt.axis('equal')
     plt.ion()

snudda_detect.py
@@ -783,7 +783,9 @@ class SnuddaDetect(object):
     # Clear lookup tables, just to be safe
     self.hyperVoxelSynapseLookup = None
     self.hyperVoxelGapJunctionLookup = None
-            
+
+    # Used by plotHyperVoxel to make sure synapses are displayed correctly
+    self.hyperVoxelOffset = None
     
     # Which axons populate the different voxels  
     if(self.axonVoxels is None):
@@ -934,13 +936,14 @@ class SnuddaDetect(object):
 
     # Sort the synapses (note sortIdx will not contain the empty rows
     # at the end.
- 
+
     self.sortSynapses()
 
     # Convert from hyper voxel local coordinates to simulation coordinates
+    # basically how many voxel steps do we need to take to go from
+    # simulationOrigo to hyperVoxelOrigo (those were not included, so add them)
     try:
-      hyperVoxelOffset = np.round((self.hyperVoxelOrigo \
-                                   -self.simulationOrigo) \
+      hyperVoxelOffset = np.round((self.hyperVoxelOrigo - self.simulationOrigo)\
                                   /self.hyperVoxelWidth).astype(int) \
                                   * self.hyperVoxelSize
 
@@ -950,6 +953,9 @@ class SnuddaDetect(object):
       
       self.hyperVoxelSynapses[:self.hyperVoxelSynapseCtr,:][:,range(2,5)]\
         += hyperVoxelOffset
+
+      # We need this in case plotHyperVoxel is called
+      self.hyperVoxelOffset = hyperVoxelOffset
       
     except:
       import traceback
@@ -975,6 +981,16 @@ class SnuddaDetect(object):
     
     self.writeLog("detectSynapses: " + str(self.hyperVoxelSynapseCtr) \
                   + " took " + str(endTime-startTime) + "s")
+
+    if(False and self.hyperVoxelSynapseCtr > 0):
+      print("First plot shows dendrites, and the voxels that were marked")
+      print("Second plot same, but for axons")
+      self.plotHyperVoxel(plotNeurons=True,drawAxons=False)
+      self.plotHyperVoxel(plotNeurons=True,drawDendrites=False)
+      
+      import pdb
+      pdb.set_trace()
+    
     
     return self.hyperVoxelSynapses[:self.hyperVoxelSynapseCtr,:]
 
@@ -2894,7 +2910,8 @@ class SnuddaDetect(object):
 
   # hyperID is just needed if we want to plotNeurons also
   
-  def plotHyperVoxel(self,plotNeurons=False,drawAxons=True,drawDendrites=True):
+  def plotHyperVoxel(self,plotNeurons=False,drawAxons=True,drawDendrites=True,
+                     detectDone=True):
     
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D  
@@ -2923,6 +2940,11 @@ class SnuddaDetect(object):
 
     if(self.hyperVoxelSynapseCtr > 0):
       sCoord = self.hyperVoxelSynapses[:self.hyperVoxelSynapseCtr,2:5]
+
+      # In case hyperVoxelOffset has been applied, we need to subtract it
+      # to draw within the hyper voxel
+      if(self.hyperVoxelOffset is not None):
+        sCoord - self.hyperVoxelOffset
       ax.scatter(sCoord[:,0],sCoord[:,1],sCoord[:,2],c="green")
     
     plt.ion()
@@ -2936,19 +2958,11 @@ class SnuddaDetect(object):
       for neuronID in self.hyperVoxels[self.hyperVoxelID]["neurons"][:nNeurons]:
         
         neuron = self.loadNeuron(self.neurons[neuronID])
-        
-        # We temp rescale to hypervoxel coordinates
-        # This modification is destructive !!! just for debugging
-        neuron.dend[:,:3] -= self.hyperVoxelOrigo
-        neuron.dend /= self.voxelSize
 
-        neuron.axon[:,:3] -= self.hyperVoxelOrigo
-        neuron.axon /= self.voxelSize
-
-        neuron.soma[:,:3] -= self.hyperVoxelOrigo
-        neuron.soma /= self.voxelSize
-
-        neuron.plotNeuron(axis=ax)
+        neuron.plotNeuron(axis=ax,
+                          plotAxon=drawAxons,
+                          plotDendrite=drawDendrites,
+                          plotOrigo=self.hyperVoxelOrigo,plotScale=1/self.voxelSize)
       
 
     plt.show()