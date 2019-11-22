fc3b365511aac69ebdb51d8545b09caa710f40c7
Johannes Hjorth <hjorth@kth.se>
2019-11-22 13:38:33 +0100

    Adding command line functionality to snudda_load.py letting the user inspect the data file

--- snudda_load.py
+++ snudda_load.py
@@ -355,7 +355,7 @@ class SnuddaLoad(object):
 
     print("Finding synapses originating from " + str(preID) + ", this is slow")
     
-    synapses = np.zeros((nMax,12))
+    synapses = np.zeros((nMax,13))
     synCtr = 0
 
     if(type(preID) == int):
@@ -384,7 +384,7 @@ class SnuddaLoad(object):
   
   # Either give preID and postID, or just postID
   
-  def findSynapses(self,preID=None,postID=None):
+  def findSynapses(self,preID=None,postID=None,silent=True):
 
     if(postID is None):
       return self.findSynapsesSLOW(preID=preID)
@@ -463,9 +463,10 @@ class SnuddaLoad(object):
           valB2 = rowEval(f["network/synapses"][idxB2+1,:],nNeurons)
 
       synapses = f["network/synapses"][idxB1:idxB2+1,:].copy()
-      
-      print("Synapse range, first " + str(idxB1) + ", last " + str(idxB2))
-      print(str(synapses))
+
+      if(not silent):
+        print("Synapse range, first " + str(idxB1) + ", last " + str(idxB2))
+        print(str(synapses))
 
       # Calculate coordinates
       synapseCoords = synapses[:,2:5] \
@@ -546,20 +547,75 @@ class SnuddaLoad(object):
   
   
 if __name__ == "__main__":
-  if(len(sys.argv) > 1):
-    nl = SnuddaLoad(sys.argv[1]) 
-  else:
-    print("No argument given, please specify a hdf5 snudda network file")
-    exit(-1)
-    #nl = SnuddaLoad("save/network-connect-voxel-pruned-synapse-file-413.hdf5")
 
+  from argparse import ArgumentParser
+  parser = ArgumentParser(description="Load snudda network file (hdf5)")
+  parser.add_argument("networkFile", help="Network file (hdf5)",type=str)
+  parser.add_argument("--listN", help="Lists neurons in network",
+                      action="store_true")
+  parser.add_argument("--listT", type=str,
+                      help="List neurons of type, --listT ? list the types.",
+                      default=None)
+  parser.add_argument("--listPre", help="List pre synaptic neurons",
+                      type=int)
+  parser.add_argument("--listPost", help="List post synaptic neurons (slow)",
+                      type=int)
+  args = parser.parse_args()
+
+  nl = SnuddaLoad(args.networkFile) 
+
+  if(args.listN):
+    print("Neurons in network: ")
+
+    for nid,name in [(x["neuronID"],x["name"]) for x in nl.data["neurons"]]:
+      print("%d : %s" % (nid,name))
+    
+  if(args.listT is not None):
+    if(args.listT == "?"):
+      print("List neuron types in network:")
+
+      nTypes = np.unique([x["type"] for x in nl.data["neurons"]])
+      for nt in nTypes:
+        num = len([x["type"] for x in nl.data["neurons"] if x["type"] == nt])
+        print(nt + " (" + str(num) + " total)")
+      
+    else:
+      print("Neurons of type " + args.listT + ":")
+      nOfType = [(x["neuronID"], x["name"]) for x in nl.data["neurons"] \
+                 if x["type"] == args.listT]
+      for nid,name in nOfType:
+        print("%d : %s" % (nid,name))
+
+  if(args.listPre):
+    print("List neurons pre-synaptic to neuronID = " + str(args.listPre) \
+          + " (" + str(nl.data["neurons"][args.listPre]["name"]) + ")" )
+    synapses = nl.findSynapses(postID=args.listPre)
+    preID = np.unique(synapses[0][:,0])
+    
+    for nid,name in [(x["neuronID"], x["name"]) for x in nl.data["neurons"] \
+                     if x["neuronID"] in preID]:
+      nSyn = np.sum(synapses[0][:,0] == nid)
+      print("%d : %s (%d synapses)" % (nid,name,nSyn))
+
+  if(args.listPost):
+    print("List neurons post-synaptic to neuronID = " + str(args.listPost) \
+          + " (" + str(nl.data["neurons"][args.listPost]["name"]) + ")" )
+    synapses = nl.findSynapses(preID=args.listPost)
+    postID = np.unique(synapses[0][:,1])
+    
+    for nid,name in [(x["neuronID"], x["name"]) for x in nl.data["neurons"] \
+                     if x["neuronID"] in postID]:
+      nSyn = np.sum(synapses[0][:,1] == nid)
+      print("%d : %s (%d synapses)" % (nid,name,nSyn))
+    
+    # List neurons of network
+  
+  
   #syn = nl.findSynapses(22,5)
 
   #syn2 = nl.findSynapses(postID=5) 
 
 
   
-  cellID = nl.getCellIDofType(neuronType="FSN")
+  # cellID = nl.getCellIDofType(neuronType="FSN")
   
-  import pdb
-  pdb.set_trace()
