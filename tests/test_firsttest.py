6c725a88a6740716e00d86ba46bc72392b8eac52
Johannes Hjorth <hjorth@kth.se>
2020-01-15 18:05:29 +0100

    Unit test test

---
+++ tests/test_firsttest.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestFirstTest(unittest.TestCase):
+
+    def test_firstTest(self):
+        self.assertTrue(True)
+        self.assertFalse(False)
+        self.assertEquals("ABC","ABC")
+
+
