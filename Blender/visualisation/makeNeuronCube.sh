63d679508048c235eff3cb91b807a17c664c02f0
Robin De Schepper <robin.deschepper93@gmail.com>
2020-01-20 01:12:56 +0100

    repository cleanup.
    Removed anything that doesn't need to be distributed to the package 
    users

--- Blender/visualisation/makeNeuronCube.sh
+++
@@ -1,4 +0,0 @@
-# First open blender and run the makeNeuronCube.py, adjust the camera,
-# then save the blend file. Render it using the command below
-# or directly in blender.
-blender -b cubewith100neurons.blend -o neuroncube.png -f 1
