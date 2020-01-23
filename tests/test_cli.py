191346905979089227167b6db57d7ea9545f3a37
Robin De Schepper <robin.deschepper93@gmail.com>
2020-01-23 13:17:48 +0100

    added tests to test a basic workflow.

---
+++ tests/test_cli.py
@@ -0,0 +1,39 @@
+import unittest, os, sys, argparse
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+import snudda.cli
+
+# Duck punch the argument parser so it doesn't sys.exit
+def on_argparse_error(self, message):
+    raise argparse.ArgumentError(None, message)
+
+
+argparse.ArgumentParser.error = on_argparse_error
+
+
+def run_cli_command(command):
+    argv = sys.argv
+    sys.argv = command.split(" ")
+    sys.argv.insert(0, "test_cli_command")
+    result = snudda.cli.snudda_cli()
+    sys.argv = argv
+    return result
+
+class TestCLI(unittest.TestCase):
+    """
+        Check if the CLI commands can be executed
+    """
+
+    def test_0_basics(self):
+        self.assertRaises(argparse.ArgumentError, run_cli_command, "doesntexist")
+
+    def test_workflow(self):
+        run_cli_command("create test-project --overwrite")
+        os.chdir('test-project')
+        run_cli_command("init tiny --size 10 --overwrite")
+        run_cli_command("place tiny")
+        run_cli_command("detect tiny --volumeID Striatum")
+        run_cli_command("prune tiny")
+        os.copy("config/input-tinytest-v5.json", "tiny/input.json")
+        run_cli_command("input tiny --input tiny/input.json --time 1.0")
+        run_cli_command("simulate tiny --time 0.1")
