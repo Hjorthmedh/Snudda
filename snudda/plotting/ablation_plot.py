b4a554f7c0c19081ea12d57362f14c251e2161f1
jofrony <jofrony@gmail.com>
2020-08-14 14:08:24 +0200

    plot on tegner

--- snudda/plotting/ablation_plot.py
+++ snudda/plotting/ablation_plot.py
@@ -1,13 +1,19 @@
-from snudda.plotting.Compare_network_activity import CompareNetwork
+from snudda.plotting.compare_frequency_activity import ComparePlotTraces
 
-fileNames = ['/afs/pdc.kth.se/home/j/jofn/Snudda/networks/TegnerRun.895338/simulation/network-output-spikes-LTS-ablation.csv',\
-            '/afs/pdc.kth.se/home/j/jofn/Snudda/networks/TegnerRun.895338/simulation/network-output-spikes-FS-ablation.csv',\
-            '/afs/pdc.kth.se/home/j/jofn/Snudda/networks/TegnerRun.895338/simulation/network-output-spikes-SPN-ablation.csv',\
-            '/afs/pdc.kth.se/home/j/jofn/Snudda/networks/TegnerRun.895338/simulation/network-output-spikes-fullnetwork.csv']
+fileNames = ['/afs/pdc.kth.se/home/j/jofn/Snudda/networks/TegnerRun.895338/simulation/volt-LTS-ablation.csv','/afs/pdc.kth.se/home/j/jofn/Snudda/networks/TegnerRun.895338/simulation/volt-FS-ablation.csv','/afs/pdc.kth.se/home/j/jofn/Snudda/networks/TegnerRun.895338/simulation/volt-SPN-ablation.csv','/afs/pdc.kth.se/home/j/jofn/Snudda/networks/TegnerRun.895338/simulation/volt-fullnetwork.csv']
+
 networkFiles = '/afs/pdc.kth.se/home/j/jofn/Snudda/networks/TegnerRun.895338/network-pruned-synapses.hdf5'
-endTime = 0.1
 
-experiment = ['LTS ablation', 'FS ablation', 'SPN ablation', 'Full network']
-CompareNetworkactivity = CompareNetwork(fileNames=fileNames,networkFiles = networkFiles,CompareNeuronType = ["dSPN","iSPN"],skipTime=0.0,
-                                  endTime=endTime,
-                                  typeOrder=["FSN","dSPN","LTS","iSPN","ChIN"])
+plotLabels = ['LTS ablation', 'FS ablation', 'SPN ablation', 'Full network']
+
+colour = ['purple','red','blue','black']
+
+Network = ComparePlotTraces(fileNames=fileNames,networkFiles=networkFile,labels=plotLabels,colours=colour)
+
+plotOffset = 0 
+skipTime = 0 
+nTracesMax = 100
+
+Network.plotTraceNeuronType(neuronType="dSPN",nTraces=nTracesMax,offset=plotOffset,skipTime=skipTime)
+Network.plotTraceNeuronType(neuronType="iSPN",nTraces=nTracesMax,offset=plotOffset,skipTime=skipTime)
+
