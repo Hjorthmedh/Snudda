fd90ec38e36f4d609fc4b6db10318fdee4f97fe6
jofrony <jofrony@gmail.com>
2020-09-07 16:17:26 +0200

    Updated for simulation of Population Unit on Beskow and Tegner

---
+++ snudda/examples/Tegner_setupInput_PopUnit_SERIAL.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+
+# !!! OBS, you need to point to the right directory which contains network!!!
+#JOBDIR=TegnerRun.${SLURM_JOBID}
+SNUDDA_DIR=/cfs/klemming/nobackup/${USER:0:1}/$USER/Snudda/snudda
+DATA=$SNUDDA_DIR/data
+JOBDIR=$SNUDDA_DIR/../networks/TegnerRun.905249
+
+if [ $SLURM_PROCID -gt 0 ]; then
+	mock_string="Not main process"
+
+else
+
+    echo ">>> Input: "`date`
+    snudda input ${JOBDIR} --input ../data/config/input-tinytest-Channel-Activation.json --time 3.5
+
+    echo "JOB END "`date` start_time_network_connect.txt
+
+fi
+
+
