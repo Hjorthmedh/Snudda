65c9e36d696c968a9ed1648c721782c66a6bbf65
Johannes Hjorth <hjorth@kth.se>
2021-01-07 15:31:55 +0100

    Updated parkinson script

--- snudda/examples/parkinsonGenerateNetwork.sh
+++ snudda/examples/parkinsonGenerateNetwork.sh
@@ -1,6 +1,9 @@
 export IPYTHONDIR="`pwd`/.ipython"
 export IPYTHON_PROFILE=Snudda_LOCAL
 
+numNeurons=3000
+simNamePart=20210107
+
 ipcluster start -n 4 --profile=$IPYTHON_PROFILE --ip=127.0.0.1&
 sleep 20
 
@@ -8,34 +11,36 @@ sleep 20
 # num_LTS=num_ChIN = 0 in init_custom.py
 
 
-simName=networks/pd0_20201218
+######################
 
-snudda init $simName --size 10000 --overwrite
+simName=networks/pd0_$simNamePart
+
+snudda init $simName --size $numNeurons --overwrite
 python3 init_custom.py $simName --cellspec data/parkinson-2020-12-17/pd0/
 snudda place $simName 
 snudda detect $simName --volumeID Striatum
 snudda prune $simName
 
-simName=networks/pd1_20201218
+simName=networks/pd1_$simNamePart
 
-snudda init $simName --size 10000 --overwrite
+snudda init $simName --size $numNeurons --overwrite
 python3 init_custom.py $simName --cellspec data/parkinson-2020-12-17/pd1/
 snudda place $simName 
 snudda detect $simName --volumeID Striatum
 snudda prune $simName
 
-simName=networks/pd2_20201218
+simName=networks/pd2_$simNamePart
 
-snudda init $simName --size 10000 --overwrite
+snudda init $simName --size $numNeurons --overwrite
 python3 init_custom.py $simName --cellspec data/parkinson-2020-12-17/pd2/
 snudda place $simName 
 snudda detect $simName --volumeID Striatum
 snudda prune $simName
 
 
-simName=networks/pd3_20201218
+simName=networks/pd3_$simNamePart
 
-snudda init $simName --size 10000 --overwrite
+snudda init $simName --size $numNeurons --overwrite
 python3 init_custom.py $simName --cellspec data/parkinson-2020-12-17/pd3/
 snudda place $simName 
 snudda detect $simName --volumeID Striatum
