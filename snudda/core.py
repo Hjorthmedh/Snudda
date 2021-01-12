02dd1e03e7e19908032ce55b1fb1d9369e60100d
Johannes Hjorth <hjorth@kth.se>
2021-01-12 06:30:52 +0100

    Updated PD-script

--- snudda/core.py
+++ snudda/core.py
@@ -33,7 +33,7 @@
 
 #
 # This open source software code was developed in part or in whole in
-# the Human Brain Project, funded from the European Union’s Horizon
+# the Human Brain Project, funded from the European Union's Horizon
 # 2020 Framework Programme for Research and Innovation under Specific
 # Grant Agreements No. 720270 and No. 785907 (Human Brain Project SGA1
 # and SGA2).
--- snudda/examples/parkinsonGenerateNetwork.sh
+++ snudda/examples/parkinsonGenerateNetwork.sh
@@ -3,6 +3,8 @@ export IPYTHON_PROFILE=Snudda_LOCAL
 
 numNeurons=3000
 simNamePart=20210107
+# cellspecDir=data/parkinson-2021-12-17
+cellspecDir=data/parkinson-2021-01-07
 
 ipcluster start -n 4 --profile=$IPYTHON_PROFILE --ip=127.0.0.1&
 sleep 20
@@ -15,24 +17,24 @@ sleep 20
 
 simName=networks/pd0_$simNamePart
 
-snudda init $simName --size $numNeurons --overwrite
-python3 init_custom.py $simName --cellspec data/parkinson-2020-12-17/pd0/
+# snudda init $simName --size $numNeurons --overwrite
+python3 init_custom.py $simName --cellspec $cellspecDir/pd0/
 snudda place $simName 
 snudda detect $simName --volumeID Striatum
 snudda prune $simName
 
 simName=networks/pd1_$simNamePart
 
-snudda init $simName --size $numNeurons --overwrite
-python3 init_custom.py $simName --cellspec data/parkinson-2020-12-17/pd1/
+# snudda init $simName --size $numNeurons --overwrite
+python3 init_custom.py $simName --cellspec $cellspecDir/pd1/
 snudda place $simName 
 snudda detect $simName --volumeID Striatum
 snudda prune $simName
 
 simName=networks/pd2_$simNamePart
 
-snudda init $simName --size $numNeurons --overwrite
-python3 init_custom.py $simName --cellspec data/parkinson-2020-12-17/pd2/
+# snudda init $simName --size $numNeurons --overwrite
+python3 init_custom.py $simName --cellspec $cellspecDir/pd2/
 snudda place $simName 
 snudda detect $simName --volumeID Striatum
 snudda prune $simName
@@ -40,8 +42,8 @@ snudda prune $simName
 
 simName=networks/pd3_$simNamePart
 
-snudda init $simName --size $numNeurons --overwrite
-python3 init_custom.py $simName --cellspec data/parkinson-2020-12-17/pd3/
+# snudda init $simName --size $numNeurons --overwrite
+python3 init_custom.py $simName --cellspec $cellspecDir/pd3/
 snudda place $simName 
 snudda detect $simName --volumeID Striatum
 snudda prune $simName
