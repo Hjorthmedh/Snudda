0c626fe65d1b6e594f43165aa6c0b1e1613d938d
Johannes Hjorth <hjorth@kth.se>
2021-02-25 15:20:29 +0100

    Fixed typo

--- snudda/cli.py
+++ snudda/cli.py
@@ -74,7 +74,7 @@ def snudda_cli():
     input_parser.add_argument("path", help="Location of network")
     input_parser.add_argument("--input", help="Input json config file (for input setup)", default=None)
     input_parser.add_argument("--inputFile", help="Input hdf5 file (for simulation)",
-                              dest="input_file", defualt=None)
+                              dest="input_file", default=None)
     input_parser.add_argument("--networkFile", help="Network file, if not network-pruned-synapses.hdf5",
                               dest="network_file")
     input_parser.add_argument("--time", type=float, default=None, help="Duration of simulation in seconds")
