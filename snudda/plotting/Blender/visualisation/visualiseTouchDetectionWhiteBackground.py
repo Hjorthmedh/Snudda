0698eb939978a47e13e2093ad487e015fad8e2b7
Johannes Hjorth <hjorth@kth.se>
2021-03-15 09:21:02 +0100

    PEPing the code

--- snudda/plotting/Blender/visualisation/visualiseTouchDetectionWhiteBackground.py
+++ snudda/plotting/Blender/visualisation/visualiseTouchDetectionWhiteBackground.py
@@ -7,187 +7,166 @@ import numpy as np
 import h5py
 from snudda.utils.snudda_path import snudda_parse_path
 
+network_file = "/home/hjorth/HBP/Snudda/snudda/examples/networks/Net10062/networ-synapses.hdf5"
+output_file = "twoNeurons-synapses-white-2020.blend"
 
-# networkFile = "/home/hjorth/HBP/StriatumNetwork/model/SmallTest2/network-connect-voxel-pruned-synapse-file.hdf5"
-#networkFile = "/home/hjorth/HBP/StriatumNetwork/model/SmallTest2/network-connect-voxel-pruned-synapse-file.hdf5"
-networkFile = "/home/hjorth/HBP/Snudda/snudda/examples/networks/Net10062/network-pruned-synapses.hdf5"
-
-outFile = "twoNeurons-synapses-white-2020.blend"
-
-#visualiseID = [20,1217]
-visualiseID = [443,7]
+visualise_id = [443, 7]
 
 # Load the neuron positions
-fi = h5py.File(networkFile,"r")
+fi = h5py.File(network_file, "r")
 
-neuronID = fi["network/neurons/neuronID"].value
-keepIdx = np.array([(x in visualiseID) for x in neuronID])
-neuronID = neuronID[keepIdx]
+neuron_id = fi["network/neurons/neuronID"].value
+keep_idx = np.array([(x in visualise_id) for x in neuron_id])
+neuron_id = neuron_id[keep_idx]
 
-pos = fi["network/neurons/position"][keepIdx,:]
-rot = fi["network/neurons/rotation"][keepIdx,:]
-morph = fi["network/neurons/morphology"][keepIdx]
-name = fi["network/neurons/name"][keepIdx]
+pos = fi["network/neurons/position"][keep_idx, :]
+rot = fi["network/neurons/rotation"][keep_idx, :]
+morph = fi["network/neurons/morphology"][keep_idx]
+name = fi["network/neurons/name"][keep_idx]
 
 origo = fi["meta/simulationOrigo"].value
-voxelSize = fi["meta/voxelSize"].value
-
+voxel_size = fi["meta/voxelSize"].value
 
 synapses = fi["network/synapses"]
 
-
 # Remove the start cube
-bpy.ops.object.delete() 
+bpy.ops.object.delete()
 
 bpy.data.scenes['Scene'].render.engine = 'CYCLES'
 world = bpy.data.worlds['World']
 world.use_nodes = True
 
-whiteBackground = True
+white_background = True
 
 # changing these values does affect the render.
 bg = world.node_tree.nodes['Background']
-if(whiteBackground): # Set to True for white background
-  bg.inputs[0].default_value[:3] = (1.0, 1.0, 1.0)
-  bg.inputs[1].default_value = 1.0
+if white_background:  # Set to True for white background
+    bg.inputs[0].default_value[:3] = (1.0, 1.0, 1.0)
+    bg.inputs[1].default_value = 1.0
 else:
-  bg.inputs[0].default_value[:3] = (0.0, 0.0, 0.0)
-  bg.inputs[1].default_value = 0.0
+    bg.inputs[0].default_value[:3] = (0.0, 0.0, 0.0)
+    bg.inputs[1].default_value = 0.0
 
-  
-matMSD1 = bpy.data.materials.new("PKHG")
-#matMSD1.diffuse_color = (1.0,0.2,0.2)
-matMSD1.diffuse_color = (77./255,151./255,1.0)
+mat_msd1 = bpy.data.materials.new("PKHG")
+mat_msd1.diffuse_color = (77. / 255, 151. / 255, 1.0)
 
-matMSD2 = bpy.data.materials.new("PKHG")
-#matMSD2.diffuse_color = (0.2,0.2,1.0)
-matMSD2.diffuse_color = (67./255,55./255,181./255)
+mat_msd2 = bpy.data.materials.new("PKHG")
+mat_msd2.diffuse_color = (67. / 255, 55. / 255, 181. / 255)
 
-matFS = bpy.data.materials.new("PKHG")
-matFS.diffuse_color = (6./255,31./255,85./255)
-matChIN = bpy.data.materials.new("PKHG")
-matChIN.diffuse_color = (252./255,102./255,0.0)
-matLTS = bpy.data.materials.new("PKHG")
-matLTS.diffuse_color = (150./255,63./255,212./255)
+mat_fs = bpy.data.materials.new("PKHG")
+mat_fs.diffuse_color = (6. / 255, 31. / 255, 85. / 255)
+mat_chin = bpy.data.materials.new("PKHG")
+mat_chin.diffuse_color = (252. / 255, 102. / 255, 0.0)
+mat_lts = bpy.data.materials.new("PKHG")
+mat_lts.diffuse_color = (150. / 255, 63. / 255, 212. / 255)
 
-matOther = bpy.data.materials.new("PKHG")
-matOther.diffuse_color = (0.4,0.4,0.4)
+mat_other = bpy.data.materials.new("PKHG")
+mat_other.diffuse_color = (0.4, 0.4, 0.4)
 
-matSynapse = bpy.data.materials.new("PKHG")
-if(whiteBackground):
-  matSynapse.diffuse_color = (0.8,0.0,0.0)
+mat_synapse = bpy.data.materials.new("PKHG")
+if white_background:
+    mat_synapse.diffuse_color = (0.8, 0.0, 0.0)
 else:
-  matSynapse.diffuse_color = (1.0,1.0,0.9)
-  
-#matSynapse.use_transparency = True
-matSynapse.use_nodes = True
-#import pdb
-#pdb.set_trace()
+    mat_synapse.diffuse_color = (1.0, 1.0, 0.9)
+
+# matSynapse.use_transparency = True
+mat_synapse.use_nodes = True
 
 
-if(not whiteBackground):
-  emissionStrength = 5.0
-  
-  # Make synapses glow
-  emission = matSynapse.node_tree.nodes.new('ShaderNodeEmission')
-  emission.inputs['Strength'].default_value = emissionStrength
+if not white_background:
+    emissionStrength = 5.0
 
-  material_output = matSynapse.node_tree.nodes.get('Material Output')
-  matSynapse.node_tree.links.new(material_output.inputs[0], emission.outputs[0])
+    # Make synapses glow
+    emission = mat_synapse.node_tree.nodes.new('ShaderNodeEmission')
+    emission.inputs['Strength'].default_value = emissionStrength
 
+    material_output = mat_synapse.node_tree.nodes.get('Material Output')
+    mat_synapse.node_tree.links.new(material_output.inputs[0], emission.outputs[0])
 
-for ps,rt,mo,nm,nID in zip(pos,rot,morph,name,neuronID):
+for ps, rt, mo, nm, n_id in zip(pos, rot, morph, name, neuron_id):
 
-  eRot = mathutils.Matrix(rt.reshape(3,3)).to_euler()
-  bpy.ops.import_mesh.swc(filepath=snudda_parse_path(mo))
-  obj = bpy.context.selected_objects[0]
-  
-  obj.rotation_euler = eRot
-  print("Setting position: " + str(ps*1e3))
-  obj.location = ps*1e3
+    e_rot = mathutils.Matrix(rt.reshape(3, 3)).to_euler()
+    bpy.ops.import_mesh.swc(filepath=snudda_parse_path(mo))
+    obj = bpy.context.selected_objects[0]
 
-  nType = nm.decode().split("_")[0]
-  if(nType == "dSPN" or nType == "MSD1"):
-    print(str(nID) + " dSPN")
-    mat = matMSD1
-  elif(nType == "iSPN" or nType == "MSD2"):
-    print(str(nID) + " iSPN")
-    mat = matMSD2
-  elif(nType == "FSN"):
-    print(str(nID) + " FSN")
-    mat = matFS
-  elif(nType == "ChIN"):
-    print(str(nID) + " ChIN")
-    mat = matChIN
-  elif(nType == "LTS"):
-    print(str(nID) + " LTS")
-    mat = matLTS
-  else:
-    print(str(nID) + " other")
-    mat = matOther
+    obj.rotation_euler = e_rot
+    print(f"Setting position: {ps * 1e3}")
+    obj.location = ps * 1e3
 
-  for ch in obj.children:
-    ch.active_material = mat
+    n_type = nm.decode().split("_")[0]
+    if n_type == "dSPN" or n_type == "MSD1":
+        print(f"{n_id} dSPN")
+        mat = mat_msd1
+    elif n_type == "iSPN" or n_type == "MSD2":
+        print(f"{n_id} iSPN")
+        mat = mat_msd2
+    elif n_type == "FSN":
+        print(f"{n_id} FSN")
+        mat = mat_fs
+    elif n_type == "ChIN":
+        print(f"{n_id} ChIN")
+        mat = mat_chin
+    elif n_type == "LTS":
+        print(f"{n_id} LTS")
+        mat = mat_lts
+    else:
+        print(f"{n_id} other")
+        mat = mat_other
 
-  obj.select = False
+    for ch in obj.children:
+        ch.active_material = mat
+
+    obj.select = False
 
 # Draw the synapses
 
-nSynapses = 0
+n_synapses = 0
 
 for ob in bpy.context.selected_objects:
-  ob.select = False
+    ob.select = False
 
 for syn in synapses:
-  preID = syn[0]
-  postID = syn[1]
+    pre_id = syn[0]
+    post_id = syn[1]
 
-  if(preID in visualiseID and postID in visualiseID):
-    # Draw this neuron (the SWC import scales from micrometers to mm), the
-    # positions in the simulation are in meters, need to scale it to mm for
-    # blender to have same units.
-    x = (origo[0] + voxelSize*syn[2]) * 1e3
-    y = (origo[1] + voxelSize*syn[3]) * 1e3
-    z = (origo[2] + voxelSize*syn[4]) * 1e3
+    if pre_id in visualise_id and post_id in visualise_id:
+        # Draw this neuron (the SWC import scales from micrometers to mm), the
+        # positions in the simulation are in meters, need to scale it to mm for
+        # blender to have same units.
+        x = (origo[0] + voxel_size * syn[2]) * 1e3
+        y = (origo[1] + voxel_size * syn[3]) * 1e3
+        z = (origo[2] + voxel_size * syn[4]) * 1e3
 
-    #bpy.ops.mesh.primitive_cube_add(location=(x,y,z))
-    #bpy.context.scene.objects.active.dimensions = \
-    #  (voxelSize, voxelSize, voxelSize)
+        bpy.ops.mesh.primitive_uv_sphere_add(location=(x, y, z), size=0.001 * 4)
+        ob = bpy.context.selected_objects[0]
+        ob.active_material = mat_synapse
+        ob.select = False
 
-    bpy.ops.mesh.primitive_uv_sphere_add(location=(x,y,z),size=0.001*4)
-    ob = bpy.context.selected_objects[0] 
-    ob.active_material = matSynapse
-    ob.select = False
-    
-    nSynapses += 1
-    
-    print("Added synapse at " + str([x,y,z]))
+        n_synapses += 1
 
-print("nSynapses = " + str(nSynapses))
-    
-#import pdb
-#pdb.set_trace()
+        print(f"Added synapse at {[x, y, z]})
+
+print(f"nSynapses = {n_synapses}")
 
 # Add a light source
 
-lampData = bpy.data.lamps.new(name="Sun", type='SUN')
-lampObject = bpy.data.objects.new(name="Sun", object_data=lampData)
-bpy.context.scene.objects.link(lampObject)
+lamp_data = bpy.data.lamps.new(name="Sun", type='SUN')
+lamp_object = bpy.data.objects.new(name="Sun", object_data=lamp_data)
+bpy.context.scene.objects.link(lamp_object)
 
 # Place lamp to a specified location
-lampObject.location = (1000.0, 1000.0, 1000.0)
+lamp_object.location = (1000.0, 1000.0, 1000.0)
 
 # Reposition camera
 cam = bpy.data.objects["Camera"]
-#cam.location = (2.98,2.68,4.96)
-#cam.rotation_euler = (1.59,0,-0.26)
+# cam.location = (2.98,2.68,4.96)
+# cam.rotation_euler = (1.59,0,-0.26)
 
-cam.location = (3.19,3.46,4.96)
-cam.rotation_euler = (96.7*np.pi/180,0,-14.3*np.pi/180)
+cam.location = (3.19, 3.46, 4.96)
+cam.rotation_euler = (96.7 * np.pi / 180, 0, -14.3 * np.pi / 180)
 
+if False:
+    bpy.ops.render.render()
+    bpy.data.images['Render Result'].save_render(filepath="cube-of-neurons-with-neurites.png")
 
-if(False):
-  bpy.ops.render.render()
-  bpy.data.images['Render Result'].save_render(filepath="cube-of-neurons-with-neurites.png")
-
-bpy.ops.wm.save_as_mainfile(filepath=outFile)
+bpy.ops.wm.save_as_mainfile(filepath=output_file)
