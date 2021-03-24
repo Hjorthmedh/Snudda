9fb48b4423aae470c4f2374da257b1b73fe276e7
Johannes Hjorth <hjorth@kth.se>
2021-03-24 17:28:04 +0100

    Updated PD scripts

--- examples/parkinsonGenerateNetwork.sh
+++ examples/parkinsonGenerateNetwork.sh
@@ -3,9 +3,10 @@ export IPYTHON_PROFILE=Snudda_LOCAL
 
 # OBS, currently init is commented out, so numneurons not used
 numNeurons=100000
-simNamePart=20210107
+simNamePart=20210319
 #cellspecDir=data/parkinson-2020-12-17
-cellspecDir=data/parkinson-2021-01-07
+#cellspecDir=data/parkinson-2021-01-07
+cellspecDir=../snudda/data/parkinson-2021-03-19
 
 ipcluster start -n 4 --profile=$IPYTHON_PROFILE --ip=127.0.0.1&
 sleep 20
@@ -19,7 +20,7 @@ sleep 20
 simName=networks/pd0_100k_$simNamePart
 
 # snudda init $simName --size $numNeurons --overwrite
-python3 init_custom.py $simName --cellspec $cellspecDir/pd0/
+python3 ../snudda/init/init_custom.py $simName --cellspec $cellspecDir/pd0/
 snudda place $simName --parallel
 snudda detect $simName --parallel
 snudda prune $simName --parallel
@@ -27,7 +28,7 @@ snudda prune $simName --parallel
 simName=networks/pd1_100k_$simNamePart
 
 # snudda init $simName --size $numNeurons --overwrite
-python3 init_custom.py $simName --cellspec $cellspecDir/pd1/
+python3 ../snudda/init/init_custom.py $simName --cellspec $cellspecDir/pd1/
 snudda place $simName --parallel
 snudda detect $simName --parallel
 snudda prune $simName --parallel
@@ -35,7 +36,7 @@ snudda prune $simName --parallel
 simName=networks/pd2_100k_$simNamePart
 
 # snudda init $simName --size $numNeurons --overwrite
-python3 init_custom.py $simName --cellspec $cellspecDir/pd2/
+python3 ../snudda/init/init_custom.py $simName --cellspec $cellspecDir/pd2/
 snudda place $simName --parallel
 snudda detect $simName --parallel
 snudda prune $simName --parallel
@@ -44,7 +45,7 @@ snudda prune $simName --parallel
 simName=networks/pd3_100k_$simNamePart
 
 # snudda init $simName --size $numNeurons --overwrite
-python3 init_custom.py $simName --cellspec $cellspecDir/pd3/
+python3 ../snudda/init/init_custom.py $simName --cellspec $cellspecDir/pd3/
 snudda place $simName --parallel
 snudda detect $simName --parallel
 snudda prune $simName --parallel
