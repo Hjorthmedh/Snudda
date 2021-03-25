e14de081fb5f93b6851fc0545ffaa5968701ccc9
Johannes Hjorth <hjorth@kth.se>
2021-03-25 15:50:09 +0100

    Trying on Tegner

--- examples/parallel/Tegner_runSnudda.sh
+++ examples/parallel/Tegner_runSnudda.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -l
 
 SNUDDA_DIR=/cfs/klemming/nobackup/${USER:0:1}/$USER/Snudda/snudda
-$DATA=$SNUDDA_DIR/data
-
-
 JOBDIR=$SNUDDA_DIR/../networks/TegnerRun.${SLURM_JOBID}
 #JOBDIR=$HOME/networks/TegnerRun.${SLURM_JOBID}
 
 #SIMSIZE=120000
-SIMSIZE=10062
-#SIMSIZE=200000
+#SIMSIZE=10062
+SIMSIZE=2000
 #SIMSIZE=1760000
+
+mkdir -p $JOBDIR
 
 if [ $SLURM_PROCID -gt 0 ]; then
 	mock_string="Not main process"
@@ -29,6 +28,18 @@ else
     echo "Start time: " > start_time_network_connect.txt
     date >> start_time_network_connect.txt
 
+    # Change to: snudda init ${JOBDIR} --size ${SIMSIZE}
+    # need to figure out how to get it ti find snudda so we can call it directly
+    # instead of calling core.py
+    echo ">>> Init: "`date`
+    snudda init ${JOBDIR} --size ${SIMSIZE} --overwrite
+
+    if [ $? != 0 ]; then
+	echo "Something went wrong during init, aborting!"
+	ipcluster stop	
+	exit -1
+    fi
+    
     #.. Obtain infiniband ip - this is faster and also internal
     /sbin/ifconfig ib0 | head -n 2 | tail -n 1 | awk '{print $2}' > controller_ip.txt
     CONTROLLERIP=$(<controller_ip.txt)
@@ -54,17 +65,6 @@ else
 
     #.. Run the self-installed version of python3
     #${ANACONDA_HOME}/bin/python3 badExample.py 
-    # Change to: snudda init ${JOBDIR} --size ${SIMSIZE}
-    # need to figure out how to get it ti find snudda so we can call it directly
-    # instead of calling core.py
-    echo ">>> Init: "`date`
-    snudda init ${JOBDIR} --size ${SIMSIZE}
-
-    if [ $? != 0 ]; then
-	echo "Something went wrong during init, aborting!"
-	ipcluster stop	
-	exit -1
-    fi
 
     echo ">>> Place: "`date`
     snudda place ${JOBDIR} --parallel
@@ -93,10 +93,10 @@ else
 	exit -1
     fi
 
-    # echo ">>> Input: "`date`
-    # snudda input ${JOBDIR} --input ../data/input_config/input-tinytest-v6.json
-    echo "Use Tegner_setupInput_SERIAL.job to generate the input spikes"
-
+    echo ">>> Input: "`date`
+    cp -a $SNUDDA_DIR/data/input_config/input-v10-scaled.json ${JOBDIR}/input.json
+    snudda input ${JOBDIR} --parallel --time 5
+    
     #.. Shut down cluster
     ipcluster stop	
 
