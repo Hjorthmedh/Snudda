92b7781e9bef451593e4c03d35d3214b1d2eb19d
Johannes Hjorth <hjorth@kth.se>
2021-04-13 15:50:35 +0200

    Moving outer point

--- snudda/place/region_mesh.py
+++ snudda/place/region_mesh.py
@@ -280,7 +280,9 @@ class RegionMesh(object):
         self.voxel_mask_inner = data["voxelMaskInner"]
         self.voxel_mask_border = data["voxelMaskBorder"]
 
-        self.point_out = self.max_coord + 1e-2
+        self.point_out = self.max_coord + np.array([1e-1, 1e-2, 1e-3])
+        # self.point_out = self.max_coord + 1e-2
+
 
         assert self.bin_width == data["binWidth"], \
             "Mismatch binWidth: " + str(self.bin_width) + " vs " + str(data["binWidth"])
@@ -386,7 +388,8 @@ class RegionMesh(object):
                 pdb.set_trace()
 
             # Used by ray casting when checking if another point is interior
-            self.point_out = self.max_coord + 1e-2
+            self.point_out = self.max_coord + np.array([1e-1, 1e-2, 1e-3])
+            # self.point_out = self.max_coord + 1e-2
 
     ############################################################################
 
@@ -539,6 +542,88 @@ class RegionMesh(object):
     # if an odd number of intersections, then it is an inner point
     #
     # Based on: https://www.erikrotteveel.com/python/three-dimensional-ray-tracing-in-python/
+
+    def ray_casting_alt(self, point):
+
+        n_tri = self.mesh_faces.shape[0]
+
+        intersect_count = 0
+
+        for i in range(0, n_tri):
+            status = self.ray_intersect_triangle(point, self.point_out, self.mesh_vec[self.mesh_faces[i, :], :])
+
+            if status == 2:
+                # On a triangle, ie inside
+                return True
+
+            intersect_count += status
+
+        return np.mod(intersect_count, 2) == 1
+
+    # Taken from: https://www.erikrotteveel.com/python/three-dimensional-ray-tracing-in-python/
+    # which is based on : source: http://geomalgorithms.com/a06-_intersect-2.html
+
+    @staticmethod
+    def ray_intersect_triangle(p0, p1, triangle):
+        # Tests if a ray starting at point p0, in the direction
+        # p1 - p0, will intersect with the triangle.
+        #
+        # arguments:
+        # p0, p1: numpy.ndarray, both with shape (3,) for x, y, z.
+        # triangle: numpy.ndarray, shaped (3,3), with each row
+        #     representing a vertex and three columns for x, y, z.
+        #
+        # returns:
+        #    0.0 if ray does not intersect triangle,
+        #    1.0 if it will intersect the triangle,
+        #    2.0 if starting point lies in the triangle.
+        v0, v1, v2 = triangle
+        u = v1 - v0
+        v = v2 - v0
+        normal = np.cross(u, v)
+        b = np.inner(normal, p1 - p0)
+        a = np.inner(normal, v0 - p0)
+
+        # Here is the main difference with the code in the link.
+        # Instead of returning if the ray is in the plane of the
+        # triangle, we set rI, the parameter at which the ray
+        # intersects the plane of the triangle, to zero so that
+        # we can later check if the starting point of the ray
+        # lies on the triangle. This is important for checking
+        # if a point is inside a polygon or not.
+
+        if b == 0.0:
+            # ray is parallel to the plane
+            if a != 0.0:
+                # ray is outside but parallel to the plane
+                return 0
+            else:
+                # ray is parallel and lies in the plane
+                r_i = 0.0
+        else:
+            r_i = a / b
+        if r_i < 0.0:
+            return 0
+        w = p0 + r_i * (p1 - p0) - v0
+        denom = np.inner(u, v) * np.inner(u, v) - np.inner(u, u) * np.inner(v, v)
+        si = (np.inner(u, v) * np.inner(w, v) - np.inner(v, v) * np.inner(w, u)) / denom
+
+        if (si < 0.0) | (si > 1.0):
+            return 0
+        ti = (np.inner(u, v) * np.inner(w, u) - np.inner(u, u) * np.inner(w, v)) / denom
+
+        if (ti < 0.0) | (si + ti > 1.0):
+            return 0
+        if r_i == 0.0:
+            # point 0 lies ON the triangle. If checking for
+            # point inside polygon, return 2 so that the loop
+            # over triangles can stop, because it is on the
+            # polygon, thus inside.
+            return 2
+        return 1
+
+    # TODO: When the line between the interior and exterior point crosses the line between two vertexes this code
+    #       might incorrectly say the line is outside by considering it crosses both lines
 
     def ray_casting(self, point):
 
@@ -792,15 +877,15 @@ class RegionMesh(object):
                 self.density_voxel_n_sample[neuron_type][vx, vy, vz] += 1
                 self.density_total_n_sample[neuron_type] += 1
 
-                # n_expected = (self.density_voxel_sum[neuron_type][vx, vy, vz]
-                #               / self.density_total_sum[neuron_type]
-                #               * (self.placed_total[neuron_type] + 1))
+                n_expected = (self.density_voxel_sum[neuron_type][vx, vy, vz]
+                              / self.density_total_sum[neuron_type]
+                              * (self.placed_total[neuron_type] + 1))
 
                 # This assumes all of mesh voxels have same volume
-                n_expected = ((self.density_voxel_sum[neuron_type][vx, vy, vz]
-                              / self.density_voxel_n_sample[neuron_type][vx, vy, vz])
-                              / (self.density_total_sum[neuron_type] / self.density_total_n_sample[neuron_type])
-                              * (self.placed_total[neuron_type] + 1))
+                # n_expected = ((self.density_voxel_sum[neuron_type][vx, vy, vz]
+                #               / self.density_voxel_n_sample[neuron_type][vx, vy, vz])
+                #               / (self.density_total_sum[neuron_type] / self.density_total_n_sample[neuron_type])
+                #               * (self.placed_total[neuron_type] + 1))
 
                 if self.placed_voxel[neuron_type][vx, vy, vz] > np.ceil(n_expected) + 1:
                     # We have too many neurons in this part of the volume already, reject
--- tests/test_place_density.py
+++ tests/test_place_density.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
 from snudda.init.init import SnuddaInit
 from snudda.place.place import SnuddaPlace
-from snudda.place import create_slice_mesh
+from snudda.place import create_slice_mesh, RegionMesh
 
 
 class SnuddaDensityTest(unittest.TestCase):
@@ -86,6 +86,44 @@ class SnuddaDensityTest(unittest.TestCase):
         # self.assertEqual(True, False)
 
         # TODO: Add a check for density -- currently it appears to not be working
+
+    @staticmethod
+    def is_inside(point):
+        if (0 <= point).all() and (point <= 2e-3).all():
+            return True
+        else:
+            return False
+
+    def test_ray_casting(self):
+
+        print("Testing ray casting algorithm")
+
+        # This sets up a small cube, then tries the ray tracing function with different exterior points,
+        # to see if they all give the same result
+
+        mesh_file = os.path.join(self.network_path, "mesh-test.obj")
+
+        create_slice_mesh(file_name=mesh_file,
+                          centre_point=[1e-3, 1e-3, 1e-3],
+                          x_len=2e-3,
+                          y_len=2e-3,
+                          z_len=2e-3,
+                          description="Test slice")
+
+        rm = RegionMesh(mesh_file, use_cache=False, bin_width=5e-4)
+
+        inside_point = rm.random_generator.uniform(low=0, high=2e-3, size=3)
+        a_point = rm.random_generator.uniform(low=-1e-3, high=3e-3, size=3)
+
+        # [0.00092975 0.00117448 0.00070073] -- check this point
+
+        for test_ctr in range(0, 1000):
+            a_point = rm.random_generator.uniform(low=-1e-3, high=3e-3, size=3)
+            self.assertEqual(rm.ray_casting(a_point), self.is_inside(a_point))
+
+            # assert rm.ray_casting(a_point) == self.is_inside(a_point), \
+            # f"{a_point} ray_casting={rm.ray_casting(a_point)} should be {self.is_inside(a_point)} for point {a_point}"
+            # print(f"Testing {a_point} ray_casting={rm.ray_casting(a_point)} ({self.is_inside(a_point)})")
 
 
 if __name__ == '__main__':
