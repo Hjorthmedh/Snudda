5661916458b3527af7131b134ae8685d48941cbd
Johannes Hjorth <hjorth@kth.se>
2021-05-19 11:15:59 +0200

    Removed blocking in del

--- snudda/utils/cleanup.py
+++ snudda/utils/cleanup.py
@@ -1,7 +1,8 @@
 def cleanup_memory(d_view, variable_list):
 
     for var in variable_list:
-        d_view.execute(f"if '{var}' in locals(): del {var}", block=True)
+        d_view.execute(f"if '{var}' in locals(): del {var}")
+        # d_view.execute(f"{var}=None")
 
 
 # Cleans up memory on workers after execution
@@ -9,10 +10,10 @@ def cleanup(rc, state):
 
     d_view = rc.direct_view(targets='all')
 
-    var_lookup = {"place": ["sm", "innerMask"],  # region_mesh.py
-                  "detect": ["sd", "min_max", "result"],
+    var_lookup = {"place": ["innerMask", "sm"],  # region_mesh.py
+                  "detect": ["min_max", "result", "sd"],
                   "project": [],  # Currently does not support parallel execution
-                  "prune": ["sp", "syn_before", "syn_after", "merge_result_syn", "merge_result_gj"]}
+                  "prune": ["syn_before", "syn_after", "merge_result_syn", "merge_result_gj", "sp"]}
 
     if state in var_lookup:
         cleanup_memory(d_view, var_lookup[state])
