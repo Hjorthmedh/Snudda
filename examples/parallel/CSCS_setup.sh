d7c337ebf6db394bf7b075320e9c068fd6abd225
Johanna Frost Nylen <bp000144@daint105.login.cscs.ch>
2021-06-21 16:09:45 +0200

    Changed to install Snudda, added tilde

--- examples/parallel/CSCS_setup.sh
+++ examples/parallel/CSCS_setup.sh
@@ -3,6 +3,7 @@ module load cray-python
 module load daint-mc
 module swap PrgEnv-cray PrgEnv-gnu
 
+pushd ~
 python3 -m venv snudda_env
 
 module rm cray-python
@@ -11,7 +12,7 @@ source snudda_env/bin/activate
 
 MPICC=cc pip install mpi4py
 pip install snudda
-
+pip install numba
 # salloc -C mc -A ich030 -n 1 -t 1:00:00
 # srun -C mc -A ich030 -n 1 --ntasks-per-node=1 ./daint-snudda-venv-build.sh
 
