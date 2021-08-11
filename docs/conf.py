99ff210a192b50c30911c9e46c37e975cc664a7b
Johannes Hjorth <hjorth@kth.se>
2021-08-11 14:25:30 +0200

    Updating paths

--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,7 @@ author = 'Johannes Hjorth'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -50,3 +49,9 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
