c73c20dba20de0114022bb5f4aa64f59d4d6a292
Johannes Hjorth <hjorth@kth.se>
2021-12-08 13:38:34 +0100

    Working on Dardel files

---
+++ examples/parallel/KTH_PDC/Dardel_install_Snudda.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -d "$HOME/Snudda" ]; then
+    echo "Directory $HOME/Snudda already exists"
+else
+    pushd $HOME
+    git clone -b master https://github.com/Hjorthmedh/Snudda.git
+    # git clone -b master git@github.com:Hjorthmedh/Snudda.git    
+fi
+
+pushd $HOME/Snudda
+git pull
+
+module load cray-python
+module load snic-env
+module swap PrgEnv-cray PrgEnv-gnu
+module unload cray-libsci atp
+
+# Setup virtual environment
+python -m venv snudda_env
+source snudda_env/bin/activate
+
+pip install --upgrade pip
+MPICC=cc pip install mpi4py
+
+pip install -r requirements.txt
+pip install -e .[dev]
+
+
+
+
+
+
