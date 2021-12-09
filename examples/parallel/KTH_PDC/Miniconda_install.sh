e3db852a6cab5345afd6b9fc639fccf4b0a5cc03
Johannes Hjorth <hjorth@kth.se>
2021-12-09 17:14:29 +0100

    Dardel simulation setup, work in progress

--- examples/parallel/KTH_PDC/Miniconda_install.sh
+++ examples/parallel/KTH_PDC/Miniconda_install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Download and install miniconda3
+# -- On Dardel compute nodes does not have wget,
+# so you have to do this manually then
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod u+x Miniconda3-latest-Linux-x86_64.sh
 
 module load snic-env
-L=/cfs/klemming/nobackup/${USER:0:1}/$USER/local/$SNIC_RESOURCE
+L=/cfs/klemming/home/${USER:0:1}/$USER/local/$SNIC_RESOURCE
 
 ./Miniconda3-latest-Linux-x86_64.sh -b -p $L/miniconda3
 
@@ -42,6 +44,14 @@ elif [ $SNIC_RESOURCE == "beskow" ]; then
     # module load gcc/10.2.0
     # module load ??? # What is openmpi module on Beskow?
    #do something
+elif [ $SNIC_RESOURCE == "dardel" ]; then
+    echo "On Beskow"
+
+    # Recompile mpi4py using MPICH
+    export MPICC=cc
+    export LD_LIBRARY_PATH=$MPICH_DIR/lib:$LD_LIBRARY_PATH
+
+
 else
     echo "Unknown system $SNIC_RESOURCE"
 fi
@@ -51,7 +61,7 @@ pip install mpi4py --ignore-installed --no-cache-dir
 
 
 
-pushd ../../
+pushd ../../../
 
 # Install Snudda -- only if you do not already have Snudda installed
 # cd $L
