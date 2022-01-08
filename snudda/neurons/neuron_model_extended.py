7255ea4a91bf785791ea516b7b7958b154af4307
jofrony <jofrony@gmail.com>
2022-01-08 14:16:13 +0100

    Remove unneccessary comment

--- snudda/neurons/neuron_model_extended.py
+++ snudda/neurons/neuron_model_extended.py
@@ -83,8 +83,6 @@ class NeuronModel(ephys.models.CellModel):
         morph = self.define_morphology(replace_axon=True, morph_file=morph_file)
         mechs = self.define_mechanisms(mechanism_config=mech_file)
         params = self.define_parameters(param_file, parameter_id, parameter_key)
-
-        print(f"output: {modulation_key}!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         
         if modulation_key is not None and modulation_key!="":
             
