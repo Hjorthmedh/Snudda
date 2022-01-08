ced5454799607845f61818e179715837edbf2a47
jofrony <jofrony@gmail.com>
2022-01-08 10:26:52 +0100

    Changed the if statement for modulation

--- snudda/neurons/neuron_model_extended.py
+++ snudda/neurons/neuron_model_extended.py
@@ -83,8 +83,8 @@ class NeuronModel(ephys.models.CellModel):
         morph = self.define_morphology(replace_axon=True, morph_file=morph_file)
         mechs = self.define_mechanisms(mechanism_config=mech_file)
         params = self.define_parameters(param_file, parameter_id, parameter_key)
-        print(f"output: {modulation_key}")
-        if modulation_file is not None:
+  
+        if modulation_file is not None and modulation_key is not None:
             mod_params = self.define_parameters(parameter_config=modulation_file, parameter_key=modulation_key)
             params = params + mod_params
 
