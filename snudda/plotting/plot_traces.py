f61e369ce2f7ae890e5c24a340ef8770bcc7e775
bosse89 <bosse89@gmail.com>
2022-02-18 10:04:33 +0100

    Plot all traces as separate images
    Adding a function that plots the trace for each neuron as a separate image and puts it in a folder

--- snudda/plotting/plot_traces.py
+++ snudda/plotting/plot_traces.py
@@ -234,12 +234,9 @@ class PlotTraces:
     ############################################################################
 
     def plot_trace_neuron_name(self, neuron_name, num_traces=1, skip_time=0, plot_offset=0, fig_name=None, num_offset=0):
-
         assert self.network_info is not None, "You need to specify networkInfo file"
-
         neuron_names = [x["name"] for x in self.network_info.data["neurons"]]
         traceID = [x[0] for x in enumerate(neuron_names) if x[1].lower() == neuron_name.lower()]
-
         num_traces = min(len(traceID), num_traces)
 
         if num_traces <= 0:
@@ -252,6 +249,92 @@ class PlotTraces:
 
         time.sleep(1)
         return fig
+        def plot_traces_sep(self, trace_id=None, offset=150e-3, colours=None, skip_time=None,
+                    title=None, fig_name=None, fig_size=None, folderName=''):
+        #Plot traces and save as separate images
+        if skip_time is not None:
+            print(f"!!! Excluding first {skip_time} s from the plot")
+
+        if not trace_id:
+            if self.network_info:
+                trace_id = [x["neuronID"] for x in self.network_info.data["neurons"]]
+            else:
+                trace_id = [x for x in self.voltage]
+        elif isinstance(trace_id, (int, np.integer)):
+            trace_id = [trace_id]
+
+        if colours is None:
+            colours = {"dSPN": (77. / 255, 151. / 255, 1.0),
+                       "iSPN": (67. / 255, 55. / 255, 181. / 255),
+                       "FS": (6. / 255, 31. / 255, 85. / 255),
+                       "ChIN": [252. / 255, 102. / 255, 0],
+                       "LTS": [150. / 255, 63. / 255, 212. / 255]}
+
+        print(f"Plotting traces: {trace_id}")
+        print(f"Plotted {len(trace_id)} traces (total {len(self.voltage)})")
+
+        import matplotlib.pyplot as plt
+
+        types_in_plot = set()
+
+        if self.network_info is not None:
+            cell_types = [n["type"] for n in self.network_info.data["neurons"]]
+            cell_id_check = [n["neuronID"] for n in self.network_info.data["neurons"]]
+            try:
+                assert (np.array([cell_id_check[x] == x for x in trace_id])).all(), \
+                    "Internal error, assume IDs ordered"
+            except:
+                import traceback
+                tstr = traceback.format_exc()
+                print(tstr)
+                print("This is strange...")
+                import pdb
+                pdb.set_trace()
+
+            cols = [colours[c] if c in colours else [0, 0, 0] for c in cell_types]
+
+        if not fig_size:
+            fig_size = (10, 5)
+
+
+        if skip_time is not None:
+            time_idx = np.where(self.time >= skip_time)[0]
+        else:
+            skip_time = 0.0
+            time_idx = range(0, len(self.time))
+
+        fig_path = os.path.join(os.path.dirname(os.path.realpath(self.network_file)), "figures")
+        if not os.path.exists(fig_path):
+            os.makedirs(fig_path)
+        if not os.path.exists(os.path.join(fig_path, str(self.ID)+folderName)):
+            os.makedirs(os.path.join(fig_path, str(self.ID)+folderName))
+        plot_count = 0
+        for r in trace_id:
+            fig = plt.figure(figsize=fig_size)
+            if r not in self.voltage:
+                print("Missing data for trace " + str(r))
+                continue
+
+            plot_count += 1
+            types_in_plot.add(self.network_info.data["neurons"][r]["type"])
+
+            plt.plot(self.time[time_idx] - skip_time,1000*self.voltage[r][time_idx] ,color='black')
+
+            plt.xlabel('Time (s)')
+            plt.ylabel('Membrane potential (mV)')
+
+            if title is None and self.input_info is not None and len(trace_id) == 1:
+                n_inputs = 0
+                for input_type in self.input_info["input"][str(trace_id[0])]:
+                    n_inputs += self.input_info["input"][str(trace_id[0])][input_type]["spikes"].shape[0]
+
+                title = f"{self.network_info.data['neurons'][trace_id[0]]['name']} receiving {n_inputs} inputs"
+            title = f"{self.network_info.data['neurons'][trace_id[r]]['name']}"
+            plt.title(title)
+            plt.tight_layout()
+            
+            plt.savefig(os.path.join(fig_path, str(self.ID)+folderName,'Network-spikes-' + str(self.ID)+'-'+str(r) +"-"+title+ ".png"))
+            plt.close(fig)
 
     ############################################################################
 
