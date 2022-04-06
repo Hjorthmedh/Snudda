ee9c0a990caf1f2d3403309dca06c3b915e9433f
Johannes Hjorth <hjorth@kth.se>
2022-04-06 11:46:56 +0200

    Fixed iterator

--- snudda/utils/load_network_simulation.py
+++ snudda/utils/load_network_simulation.py
@@ -227,7 +227,9 @@ class SnuddaLoadNetworkSimulation:
         return neuron_type
 
     def iter_neuron_type(self):
-        neuron_types = list(set(self.get_neuron_type())).sort()
+        neuron_types = list(set(self.get_neuron_type()))
+        neuron_types.sort()
+                
         for x in neuron_types:
             yield x
 
