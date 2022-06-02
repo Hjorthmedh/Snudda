33f7a7ac77eac3485c751d00b4b85d6ea29d50b0
Johannes Hjorth <hjorth@kth.se>
2022-06-02 15:07:28 +0200

    Added reference line

--- snudda/analyse/analyse_topology_activity.py
+++ snudda/analyse/analyse_topology_activity.py
@@ -174,6 +174,7 @@ class SnuddaAnalyseTopologyActivity:
         plt.figure()
         plt.hist2d(x=np.array(x), y=np.array(y),
                    bins=int(np.ceil(duration/bin_size)+1), range=[[0, duration], [0, duration]])
+        plt.plot([0, duration], [0, duration], color="k")
         plt.colorbar()
         plt.xlabel("Ablated: time (s)")
         plt.ylabel("Fully ablated: time (s)")
