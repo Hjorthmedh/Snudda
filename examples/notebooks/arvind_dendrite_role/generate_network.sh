6e1c976ea1f62e8428a81c861c13e26e9fc893a2
Johannes Hjorth <hjorth@kth.se>
2022-09-22 12:47:15 +0200

    Updated shell script

--- examples/notebooks/arvind_dendrite_role/generate_network.sh
+++ examples/notebooks/arvind_dendrite_role/generate_network.sh
@@ -1,14 +1,8 @@
 export IPYTHONDIR="`pwd`/.ipython"
 export IPYTHON_PROFILE=default
 
-# If the BasalGangliaData directory exists, then use that for our data
-if [[ -d "../../BasalGangliaData/data" ]]; then
-    export SNUDDA_DATA="../../BasalGangliaData/data"
-    echo "Setting SNUDDA_DATA to $SNUDDA_DATA"
-else
-    echo "SNUDDA_DATA environment variable not changed (may be empty): $SNUDDA_DATA"
-fi
-    
+echo "This code assumes you have BasalGangliaData installed"
+
 ipcluster start -n 2 --profile=$IPYTHON_PROFILE --ip=127.0.0.1&
 sleep 20
 
@@ -17,7 +11,7 @@ for sim_size in 1000 5000 10000 50000; do
 
     sim_name=networks/arvind-$sim_size
 
-    snudda init $sim_name --size $sim_size --overwrite
+    snudda init $sim_name --size $sim_size --overwrite --snudda_data ../../../../BasalGangliaData/data --seed 123456
     snudda place $sim_name --parallel
     snudda detect $sim_name --volumeID Striatum --parallel
     snudda prune $sim_name --parallel
