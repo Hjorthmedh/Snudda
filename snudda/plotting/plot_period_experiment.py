7d6e5af9b4e2f78d5549711397de635ef6af08a0
Johannes Hjorth <hjorth@kth.se>
2022-11-01 17:35:06 +0100

    Updated plotting

--- snudda/plotting/plot_period_experiment.py
+++ snudda/plotting/plot_period_experiment.py
@@ -91,10 +91,15 @@ class PlotPeriodExperiment(SnuddaPlotSpikeRaster2):
         # ax
         return ax, freq, bins
 
-    def plot_period_voltage(self, period, neuron_type, ax=None, fig_size=None, figure_name=None, show_plot=True):
+    def plot_period_voltage(self, period, neuron_type, ax=None, fig_size=None, figure_name=None, show_plot=True,
+                            time_range=None):
 
         """ This plots the average voltage for the neurons during the spikes, split into two groups. Those that
             had a presynaptic neuron spike before, and those that did not. """
+
+        if ax is None:
+            fig = plt.figure(figsize=fig_size)
+            ax = fig.add_subplot()
 
         post_id_list = self.snudda_load.get_neuron_id_of_type(neuron_type=neuron_type)
 
@@ -105,29 +110,89 @@ class PlotPeriodExperiment(SnuddaPlotSpikeRaster2):
         no_spike_with_pre_spike = []
         no_spike_no_pre_spike = []
 
-        time = self.snudda_simulation_load.get_time()
-        period_start = np.arange(0, time[-1], period)
-        period_end = period_start + period
+        time = np.around(self.snudda_simulation_load.get_time(), 8)
+        period_start = np.around(np.arange(0, time[-1], period), 8)  # Use around to avoid rounding errors
+        period_end = np.around(period_start + period, 8)
+
+        n_periods = int(np.ceil(time[-1] / period))
+        spike_in_period = np.zeros((self.snudda_load.data["nNeurons"], n_periods))
+
+        for neuron_id in self.snudda_load.get_neuron_id():
+            spikes = self.snudda_simulation_load.get_spikes(neuron_id=neuron_id)
+
+            for spike in spikes.flatten():
+                p_idx = int(spike / period)
+                spike_in_period[neuron_id, p_idx] += 1
 
         for post_id in post_id_list:
             synapses, _ = self.snudda_load.find_synapses(post_id=post_id)
-            pre_id_lookup[post_id] = set(synapses[:, 0])
+            pre_id_lookup[post_id] = np.array(sorted(list(set(synapses[:, 0]))))
 
         for post_id in post_id_list:
-            spikes = self.snudda_simulation_load.get_spikes(neuron_id=post_id)
-            pre_spikes = self.snudda_simulation_load.get_spikes(neuron_id=pre_id_lookup[post_id])
 
-            for p_start, p_end in zip(period_start, period_end):
+            pre_id = pre_id_lookup[post_id]
+            has_pre_spike = np.sum(spike_in_period[pre_id, :], axis=0)
+            has_post_spike = spike_in_period[post_id, :]
 
-                # TODO: TO BE CONTINUED...
-                # Extract all the traces where a presynaptic neuron spikes.
-                pass
+            voltage = self.snudda_simulation_load.get_voltage(neuron_id=post_id).flatten()
 
-        assert False, "Not implemented yet"
+            for t_start, t_end, pre_spikes, post_spikes \
+                    in zip(period_start, period_end, has_pre_spike, has_post_spike):
 
+                if time_range and not (time_range[0] <= t_start and t_end <= time_range[1]):
+                    continue
 
+                t_idx = np.where(np.logical_and(t_start <= time, time < t_end))[0]
 
+                if len(t_idx) > 2000:
 
-        if ax is None:
-            fig = plt.figure(figsize=fig_size)
-            ax = fig.add_subplot()
+                    print("Tell me why?!")
+                    import pdb
+                    pdb.set_trace()
+
+                if pre_spikes:
+                    if post_spikes:
+                        spike_with_pre_spike.append(voltage[t_idx])
+                    else:
+                        no_spike_with_pre_spike.append(voltage[t_idx])
+                else:
+                    if post_spikes:
+                        spike_no_pre_spike.append(voltage[t_idx])
+                    else:
+                        no_spike_no_pre_spike.append(voltage[t_idx])
+
+        t = time[t_idx] - time[t_idx[0]]
+
+        if len(spike_with_pre_spike) > 0:
+            spike_with_pre_spike_mat = np.stack(spike_with_pre_spike, axis=1)
+            ax.plot(t * 1e3, 1e3 * np.mean(spike_with_pre_spike_mat, axis=1),
+                    label=f"{neuron_type} spike, pre-spike (n={spike_with_pre_spike_mat.shape[1]})")
+
+        if len(no_spike_with_pre_spike) > 0:
+            no_spike_with_pre_spike_mat = np.stack(no_spike_with_pre_spike, axis=1)
+            ax.plot(t * 1e3, 1e3 * np.mean(no_spike_with_pre_spike_mat, axis=1),
+                    label=f"No {neuron_type} spike, pre-spike (n={no_spike_with_pre_spike_mat.shape[1]})")
+
+        if len(spike_no_pre_spike) > 0:
+            spike_no_pre_spike_mat = np.stack(spike_no_pre_spike, axis=1)
+            ax.plot(t * 1e3, 1e3 * np.mean(spike_no_pre_spike_mat, axis=1),
+                    label=f"{neuron_type} spike, no pre-spike (n={spike_no_pre_spike_mat.shape[1]})")
+
+        if len(no_spike_no_pre_spike) > 0:
+            no_spike_no_pre_spike_mat = np.stack(no_spike_no_pre_spike, axis=1)
+            ax.plot(t*1e3, 1e3*np.mean(no_spike_no_pre_spike_mat, axis=1),
+                    label=f"No {neuron_type} spike, no pre-spike (n={no_spike_no_pre_spike_mat.shape[1]})")
+
+        ax.legend()
+        plt.xlabel("Time (ms)")
+        plt.ylabel("Voltage (mV)")
+
+        if figure_name:
+            fig_path = os.path.join(self.figure_path, figure_name)
+            plt.savefig(fig_path)
+
+        if show_plot:
+            plt.ion()
+            plt.show()
+
+        return ax
