b06dc3365d5fea65d8c6c44b15e6f4961bd1fe41
bosse89 <bosse89@gmail.com>
2022-11-09 07:40:35 +0100

    Revert "Example correlation simulation and plot"
    This reverts commit df033b3983995f372284dd83394b09890519d222.

--- examples/python-scripts/correlation/run_corr_sim.sh
+++
@@ -1,4 +0,0 @@
-mpiexec -n 3 snudda simulate networks/testnetwork6_pd0 --time 1.0
-mpiexec -n 3 snudda simulate networks/testnetwork6_pd2 --time 1.0
-#mpiexec -n 10 snudda simulate networks/testnetwork4_pd0 --time 12
-#mpiexec -n 10 snudda simulate networks/testnetwork4_pd2 --time 12
