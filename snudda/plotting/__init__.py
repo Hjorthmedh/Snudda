d3acd0e405b2d2dc95ceea78e25f1233d3469e98
bosse89 <bosse89@gmail.com>
2022-11-09 07:40:27 +0100

    Revert "Example current inj and input output sim and plot"
    This reverts commit 42d0c07a6b7d5c5e7df9f86c27204213797e2dc7.

--- snudda/plotting/__init__.py
+++ snudda/plotting/__init__.py
@@ -3,6 +3,4 @@ from snudda.plotting.plot_network import PlotNetwork
 from snudda.plotting.plot_spike_raster_v2 import SnuddaPlotSpikeRaster2
 from snudda.plotting.plot_traces import PlotTraces
 from snudda.plotting.plot_density import PlotDensity
-from snudda.plotting.plot_period_experiment import PlotPeriodExperiment
 import snudda.plotting.Blender
-from snudda.plotting.plot_input_vs_output_cur import PlotInputVsOutput
