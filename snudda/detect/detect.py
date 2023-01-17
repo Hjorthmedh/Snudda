c00355e7e46fc5ebdfc080981a0ce4b23513c2e4
Johannes Hjorth <hjorth@kth.se>
2023-01-17 15:19:34 +0100

    Cleanup

--- snudda/detect/detect.py
+++ snudda/detect/detect.py
@@ -2684,46 +2684,6 @@ class SnuddaDetect(object):
                                                                       self_step_multiplier=self.step_multiplier)
             self.voxel_overflow_counter += voxel_overflow_ctr
 
-    def fill_voxels_dend_OLD2(self, voxel_space, voxel_space_ctr,
-                         voxel_sec_id, voxel_sec_x,
-                         voxel_soma_dist,
-                         coords, links,
-                         seg_id, seg_x, neuron_id):
-
-        """
-        Mark all voxels containing dendrites.
-
-        voxel_space : n x n x n x k matrix holding the voxel content, normally self.dend_voxels (neuron IDs)
-        voxel_space_ctr : n x n x n matrix holding count of how many items each voxel holds
-        voxel_sec_id : n x n x n x k matrix, holding section ID of each item
-        voxel_sec_x : n x n x n x k matrix, holding section X of each item
-        voxel_soma_dist : n x n x n x k matrix, holding distance to soma along dendrite
-        coords : neuron vertices, n x 3 matrix
-        links : how do vertices link up to for dendrite segments n x 2 matrix
-        seg_id : segment ID of the links end points
-        seg_x : segment X of the links end points
-        neuron_id : ID of the neurons
-
-        """
-
-        voxel_overflow_ctr = SnuddaDetect.fill_voxels_dend_helper(voxel_space=voxel_space,
-                                                                  voxel_space_ctr=voxel_space_ctr,
-                                                                  voxel_sec_id=voxel_sec_id,
-                                                                  voxel_sec_x=voxel_sec_x,
-                                                                  voxel_soma_dist=voxel_soma_dist,
-                                                                  coords=coords,
-                                                                  links=links,
-                                                                  seg_id=seg_id,
-                                                                  seg_x=seg_x,
-                                                                  neuron_id=neuron_id,
-                                                                  self_hyper_voxel_origo=self.hyper_voxel_origo,
-                                                                  self_voxel_size=self.voxel_size,
-                                                                  self_num_bins=self.num_bins,
-                                                                  self_max_dend=self.max_dend,
-                                                                  self_step_multiplier=self.step_multiplier)
-
-        self.voxel_overflow_counter += voxel_overflow_ctr
-
     # Temporarily disabling NUMBA, since amax does not support axis in NUMBA
     @staticmethod
     @jit(nopython=True, fastmath=True, cache=True)
@@ -2836,83 +2796,6 @@ class SnuddaDetect(object):
 
         return self_voxel_overflow_counter
 
-    @staticmethod
-    @jit(nopython=True, fastmath=True, cache=True)
-    def fill_voxels_dend_helper_OLD(voxel_space, voxel_space_ctr,
-                                voxel_sec_id, voxel_sec_x,
-                                voxel_soma_dist,
-                                coords, links,
-                                seg_id, seg_x, neuron_id,
-                                self_hyper_voxel_origo, self_voxel_size,
-                                self_num_bins, self_max_dend,
-                                self_step_multiplier):
-
-        """ Helper function for fill_voxels_dend, static method needed for NUMBA. """
-
-        self_voxel_overflow_counter = 0
-
-        padding = 1
-        lower_padding_bound = 0 - padding
-        upper_padding_bound = self_num_bins + padding
-
-        zero_step = np.zeros((3,), dtype=np.float64)
-
-        for line, segment_id, segment_x in zip(links, seg_id, seg_x):
-            p1 = coords[line[0], :3]
-            p2 = coords[line[1], :3]
-            p1_dist = coords[line[0], 4] * 1e6  # Dist to soma
-            p2_dist = coords[line[1], 4] * 1e6
-
-            vp1 = np.floor((p1 - self_hyper_voxel_origo) / self_voxel_size).astype(np.int64)
-            vp2 = np.floor((p2 - self_hyper_voxel_origo) / self_voxel_size).astype(np.int64)
-
-            vp1_inside = ((lower_padding_bound <= vp1).all() and (vp1 < upper_padding_bound).all())
-            vp2_inside = ((lower_padding_bound <= vp2).all() and (vp2 < upper_padding_bound).all())
-
-            if vp1_inside or vp2_inside:
-                # Either of the points are within the cube + padding zone
-                steps = max(np.abs(vp2 - vp1)) * self_step_multiplier
-
-                if steps > 0:
-                    dv = (vp2 - vp1) / steps
-                    ds = (segment_x[1] - segment_x[0]) / steps
-                    dd = (p2_dist - p1_dist) / steps
-                else:
-                    # Just a point, we dont need a step
-                    dv = zero_step
-                    ds = 0
-                    dd = 0
-
-                # We want the end element "steps" also, hence +1
-
-                for i in range(0, steps + 1):
-                    vp = (vp1 + dv * i).astype(np.int64)
-                    s_x = segment_x[0] + ds * i  # float
-                    soma_dist = int(p1_dist + dd * i)
-
-                    if (0 <= vp).all() and (vp < self_num_bins).all():
-                        # Inside the hyper voxel
-                        v_ctr = voxel_space_ctr[vp[0], vp[1], vp[2]]
-
-                        if v_ctr > 0 and voxel_space[vp[0], vp[1], vp[2], v_ctr - 1] == neuron_id:
-                            # Voxel already contains neuronID, skip
-                            continue
-
-                        if v_ctr < self_max_dend:
-                            voxel_space[vp[0], vp[1], vp[2], v_ctr] = neuron_id
-                            voxel_sec_id[vp[0], vp[1], vp[2], v_ctr] = segment_id
-                            voxel_sec_x[vp[0], vp[1], vp[2], v_ctr] = s_x
-                            voxel_soma_dist[vp[0], vp[1], vp[2], v_ctr] = soma_dist
-
-                            voxel_space_ctr[vp[0], vp[1], vp[2]] += 1
-
-                        else:
-                            # self.write_log("!!! If you see this you need to increase max_dend above "
-                            #                f"{voxel_space_ctr[vp[0], vp[1], vp[2]]}", is_error=True)
-                            self_voxel_overflow_counter += 1
-
-        return self_voxel_overflow_counter
-
     ############################################################################
 
     def fill_voxels_axon(self, voxel_space, voxel_space_ctr,
@@ -2943,38 +2826,6 @@ class SnuddaDetect(object):
                                                                       self_max_axon=self.max_axon,
                                                                       self_step_multiplier=self.step_multiplier)
             self.voxel_overflow_counter += voxel_overflow_ctr
-
-    def fill_voxels_axon_OLD2(self, voxel_space, voxel_space_ctr,
-                         voxel_axon_dist,
-                         coords, links,
-                         neuron_id):
-
-        """
-        Mark all voxels containing axons.
-
-        voxel_space : n x n x n x k matrix holding the voxel content, normally self.axon_voxels (neuron IDs)
-        voxel_space_ctr : n x n x n matrix holding count of how many items each voxel holds
-        coords : neuron vertices, n x 3 matrix
-        links : how do vertices link up to for axon segments n x 2 matrix
-        neuron_id : ID of the neurons
-
-        """
-
-        # TODO: Can numba handle iterators?
-
-        voxel_overflow_ctr = SnuddaDetect.fill_voxels_axon_helper(voxel_space=voxel_space,
-                                                                  voxel_space_ctr=voxel_space_ctr,
-                                                                  voxel_axon_dist=voxel_axon_dist,
-                                                                  coords=coords,
-                                                                  links=links,
-                                                                  neuron_id=neuron_id,
-                                                                  self_hyper_voxel_origo=self.hyper_voxel_origo,
-                                                                  self_voxel_size=self.voxel_size,
-                                                                  self_num_bins=self.num_bins,
-                                                                  self_max_axon=self.max_axon,
-                                                                  self_step_multiplier=self.step_multiplier)
-
-        self.voxel_overflow_counter += voxel_overflow_ctr
 
     # Temporarily disabling NUMBA, since amax does not support axis in NUMBA
     @staticmethod
@@ -3078,76 +2929,6 @@ class SnuddaDetect(object):
                             voxel_space_ctr[v_idx] += 1
                         else:
                             # Overflow, not enough space to store info
-                            self_voxel_overflow_counter += 1
-
-        return self_voxel_overflow_counter
-
-
-    @staticmethod
-    @jit(nopython=True, fastmath=True, cache=True)
-    def fill_voxels_axon_helper_OLD2(voxel_space,
-                                voxel_space_ctr,
-                                voxel_axon_dist,
-                                coords,
-                                links,
-                                neuron_id,
-                                self_hyper_voxel_origo,
-                                self_voxel_size,
-                                self_num_bins,
-                                self_max_axon,
-                                self_step_multiplier):
-
-        """ Helper function to mark axon voxels, needed for NUMBA. See fill_voxels_axon."""
-
-        self_voxel_overflow_counter = 0
-        padding = 1
-        lower_padding_bound = 0 - padding
-        upper_padding_bound = self_num_bins + padding
-
-        zero_step = np.zeros((3,), dtype=np.float64)
-
-        for line in links:
-            p1 = coords[line[0], :3]
-            p2 = coords[line[1], :3]
-            p1_dist = coords[line[0], 4] * 1e6  # Dist to soma
-            p2_dist = coords[line[1], 4] * 1e6
-
-            vp1 = np.floor((p1 - self_hyper_voxel_origo) / self_voxel_size).astype(np.int64)
-            vp2 = np.floor((p2 - self_hyper_voxel_origo) / self_voxel_size).astype(np.int64)
-
-            vp1_inside = ((lower_padding_bound <= vp1).all() and (vp1 < upper_padding_bound).all())
-            vp2_inside = ((lower_padding_bound <= vp2).all() and (vp2 < upper_padding_bound).all())
-
-            if vp1_inside or vp2_inside:
-                steps = max(np.abs(vp2 - vp1)) * self_step_multiplier
-
-                if steps > 0:
-                    dv = (vp2 - vp1) / steps
-                    dd = (p2_dist - p1_dist) / steps
-                else:
-                    dv = zero_step
-                    dd = 0
-
-                # We want the end element "steps" also, hence +1
-                for i in range(0, steps + 1):
-                    vp = (vp1 + dv * i).astype(np.int64)
-                    ax_dist = int(p1_dist + dd * i)
-
-                    if (0 <= vp).all() and (vp < self_num_bins).all():
-
-                        v_ctr = voxel_space_ctr[vp[0], vp[1], vp[2]]
-                        if v_ctr > 0 and voxel_space[vp[0], vp[1], vp[2], v_ctr - 1] == neuron_id:
-                            # Voxel already has neuronID, skip
-                            continue
-
-                        if v_ctr < self_max_axon:
-                            voxel_space[vp[0], vp[1], vp[2], v_ctr] = neuron_id
-                            voxel_axon_dist[vp[0], vp[1], vp[2], v_ctr] = ax_dist
-
-                            voxel_space_ctr[vp[0], vp[1], vp[2]] += 1
-                        else:
-                            # self.write_log("!!! If you see this you need to increase max_axon above "
-                            #                f"{voxel_space_ctr[vp[0], vp[1], vp[2]]}", is_error=True)
                             self_voxel_overflow_counter += 1
 
         return self_voxel_overflow_counter
