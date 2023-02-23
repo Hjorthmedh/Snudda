c047eaf21778c4b3725770a5141a6ab16967f893
Johannes Hjorth <hjorth@kth.se>
2023-02-23 16:00:28 +0100

    Working on optimisation

--- snudda/optimise/optimise_pruning.py
+++ snudda/optimise/optimise_pruning.py
@@ -1,9 +1,17 @@
+import os
 import json
 import collections
 import numpy as np
-from scipy.spatial import distance_matrix
+import h5py
+
 from snudda.utils import SnuddaLoad
 from snudda.detect import SnuddaPrune
+
+from scipy.stats import binomtest
+from scipy.spatial import distance_matrix
+from scipy.optimize import differential_evolution
+
+import uuid
 
 
 class OptimisePruning:
@@ -13,6 +21,7 @@ class OptimisePruning:
 
     def __init__(self, network_path, pop_size=10, epochs=10):
 
+        self.network_path = network_path
         self.pop_size = pop_size
         self.epochs = epochs
 
@@ -24,6 +33,8 @@ class OptimisePruning:
         self.merge_files_gj = None
         self.merge_neuron_range_gj = None
         self.merge_gj_ctr = None
+
+        self.optimisation_info = dict()
 
     def merge_putative_synapses(self):
 
@@ -50,7 +61,6 @@ class OptimisePruning:
         self.merge_files_gj = merge_files_gj
         self.merge_neuron_range_gj = merge_neuron_range_gj
         self.merge_gj_ctr = merge_gj_ctr
-
 
     def prune_synapses(self, pre_type, post_type, con_type, pruning_parameters, output_file):
 
@@ -80,13 +90,22 @@ class OptimisePruning:
         self.prune.connectivity_distributions = dict([])
         self.prune.connectivity_distributions[pre_type_id, post_type_id, synapse_type_id] = (pruning, pruning_other)
 
-        # We need to make sure that we keep the pruned data files separate
-        self.prune.prune_synapses(synapse_file=self.merge_files_syn,
-                                  output_filename=output_file,
-                                  row_range=None,
-                                  close_out_file=False,
-                                  close_input_file=True,
-                                  merge_data_type="synapses")
+        assert len(self.merge_files_syn) == 1, f"merge_Files_syn should be a list with one file only"
+
+        with h5py.File(self.merge_files_syn[0], "r") as f_syn:
+            print(f"Writing file {output_file}")
+            # We need to make sure that we keep the pruned data files separate
+            self.prune.prune_synapses(synapse_file=f_syn,
+                                      output_filename=output_file,
+                                      row_range=None,
+                                      close_out_file=True,
+                                      close_input_file=True,
+                                      merge_data_type="synapses")
+
+            if not os.path.exists(output_file):
+                print(f"Output file missing {output_file}")
+                import pdb
+                pdb.set_trace()
 
     def evaluate_fitness(self, pre_type, post_type, output_file, experimental_data):
 
@@ -96,10 +115,11 @@ class OptimisePruning:
                 pre_type
                 post_type
                 output_file: path to output file from prune
-                experiment_data: [(bin start, bin end, n_con_pairs, n_tot_pairs, P)]
+                experiment_data: [(bin start, bin end, P)]
 
         """
 
+        print(f"Opening {output_file}")
         snudda_load = SnuddaLoad(network_file=output_file)
         snudda_data = snudda_load.data
 
@@ -126,40 +146,62 @@ class OptimisePruning:
         n_total = np.zeros((len(experimental_data),), dtype=int)
 
         for dist, con in zip(dist_matrix.flatten(), connection_matrix.flatten()):
-            for idx, (bin_start, bin_end, _, _, _) in enumerate(experimental_data):
-                n_total[idx] += 1
+            for idx, (bin_start, bin_end, _) in enumerate(experimental_data):
                 if bin_start <= dist <= bin_end:
-                    n_connected[idx] += 1
+                    n_total[idx] += 1
+                    if con > 0:
+                        n_connected[idx] += 1
 
+        p_hyp = np.zeros((len(experimental_data), ))
 
+        for idx, (n_con, n_tot, p_exp) in enumerate(zip(n_connected, n_total, [x[2] for x in experimental_data])):
+            # test = binomtest(n_con, n_tot, p_exp)
+            # p_hyp[idx] = test.pvalue  # gave 0 when p values are too far apart, not informative
+            p_hyp[idx] = abs(n_con/n_tot - p_exp)
 
-        # !!! TODO: Calculate the number of connected and non-connected pairs in the experimental bins
-        #           and then calculate how well the model data fits the original experimental data
+        return np.mean(p_hyp)
 
+    def helper_func(self, x):
 
+        pruning_parameters = dict()
+        pruning_parameters |= self.optimisation_info["extra_pruning_parameters"]
 
-    def
-        # For plotting
-        n_bins = 10
-        bin_size = 20
-        connected_pairs = np.zeros((n_bins, ))
-        total_pairs = np.zeros((n_bins, ))
+        pruning_parameters["f1"] = x[0]
+        pruning_parameters["softMax"] = x[1]
+        pruning_parameters["mu2"] = x[2]
+        pruning_parameters["a3"] = x[3]
 
-        for connected, d in zip(connection_matrix.flatten(), dist_matrix.flatten()):
+        output_file = os.path.join(self.network_path, f"network-synapses-{uuid.uuid4()}.hdf5")
+        print(f"Output file {output_file}")
 
-            bin = int(d / bin_size)
+        self.prune_synapses(pre_type=self.optimisation_info["pre_type"],
+                            post_type=self.optimisation_info["post_type"],
+                            con_type=self.optimisation_info["con_type"],
+                            pruning_parameters=pruning_parameters,
+                            output_file=output_file)
 
-            if bin < n_bins:
-                if connected:
-                    connected_pairs[bin] += 1
+        fitness = self.evaluate_fitness(pre_type=self.optimisation_info["pre_type"],
+                                        post_type=self.optimisation_info["post_type"],
+                                        output_file=output_file,
+                                        experimental_data=self.optimisation_info["exp_data"])
 
-                total_pairs[bin] += 1
+        self.optimisation_info["ctr"] += 1
 
+        print(f"({self.optimisation_info['ctr'] }) Evaluating f1 = {x[0]}, SM = {x[1]}, mu2 = {x[2]}, a3 = {x[3]}, fitness: {fitness}")
 
+        return fitness
 
+    def optimize(self, pre_type, post_type, con_type, experimental_data, extra_pruning_parameters):
 
-        """ Evaluate the fitness of the connection between pre_type and post_type """
+        self.optimisation_info["pre_type"] = pre_type
+        self.optimisation_info["post_type"] = post_type
+        self.optimisation_info["con_type"] = con_type
+        self.optimisation_info["exp_data"] = experimental_data
+        self.optimisation_info["extra_pruning_parameters"] = extra_pruning_parameters
+        self.optimisation_info["ctr"] = 0
 
+        bounds = [(0, 1), (0, 20), (0, 5), (0, 1)]
 
-        pass
+        res = differential_evolution(func=self.helper_func, bounds=bounds)
 
+        return res
