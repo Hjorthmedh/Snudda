Scaling fixed

snudda/place/region_mesh_redux.py
@@ -14,7 +14,7 @@ class RegionMeshRedux:
 
         # Convert from micrometers to meters to get SI units
         scale_factor = 1e-6
-        self.mesh.scale(scale_factor, center=self.mesh.get_center()*scale_factor)
+        self.mesh.scale(scale_factor, center=(0, 0, 0))
 
         self.min_coord = self.mesh.get_min_bound()
         self.max_coord = self.mesh.get_max_bound()
@@ -22,6 +22,8 @@ class RegionMeshRedux:
         self.scene = o3d.t.geometry.RaycastingScene()
         legacy_mesh = o3d.t.geometry.TriangleMesh.from_legacy(self.mesh)
         self.scene.add_triangles(legacy_mesh)
+
+
 
     def check_inside(self, points):
 
@@ -43,20 +45,39 @@ class RegionMeshRedux:
 
 class NeuronPlacer:
 
-    # Improvement suggestion. Randomize only points within a mesh-voxel, then randomise
-    # within which of the mesh voxels the points should be placed.
+    def __init__(self, mesh_path: str, d_min: float, seed=None, rng=None, n_putative_points=1000000):
 
-    def __init__(self, region_mesh: RegionMeshRedux, d_min: float, seed=None):
-
-        self.region_mesh = region_mesh
+        self.region_mesh = RegionMeshRedux(mesh_path=mesh_path)
         self.d_min = d_min
 
-        self.rng = np.random.default_rng(seed)
+        if rng:
+            if seed:
+                raise ValueError("If rng is set, then seed should not be set.")
+            self.rng = rng
+        else:
+            self.rng = np.random.default_rng(seed)
 
         # We generate a cube of points, obs that we pad it with d_min on all sides to avoid
         # edge effects (which would give higher densities at borders)
         self.cube_side = self.region_mesh.max_coord-self.region_mesh.min_coord + self.d_min*2
         self.cube_offset = self.region_mesh.min_coord - self.d_min
+
+        putative_points = self.get_point_cloud(n=n_putative_points)
+        putative_points = self.remove_close_neurons(putative_points)
+        putative_points = self.remove_outside(putative_points)
+
+        self.putative_points = putative_points
+        self.allocated_points = None
+
+    def plot_putative_points(self):
+
+        import matplotlib.pyplot as plt
+
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        x, y, z = self.putative_points.T
+        ax.scatter(x, y, z, marker='.')
+        plt.show()
 
     def get_point_cloud(self, n):
         points = self.rng.uniform(size=(n, 3), low=0, high=self.cube_side) + self.cube_offset
@@ -88,8 +109,7 @@ class NeuronPlacer:
                                        self.rng.integers(low=0, high=2, size=close_neurons.shape[0])]
 
         else:
-
-            # First remove worst offenders, with the most neighbours
+            # First remove the worst offenders, with the most neighbours
             # (but none with count 1, we do those separately
             sort_idx = np.argsort(-counts)
             sorted_offenders = unique[sort_idx]
@@ -97,7 +117,6 @@ class NeuronPlacer:
 
             first_pair = np.argmax(sorted_counts == 1)
             remove_fraction_idx = int(np.ceil(remove_fraction*len(sorted_offenders)))
-
             remove_idx = sorted_offenders[:min(first_pair, remove_fraction_idx)]
 
         points = np.delete(points, remove_idx, axis=0)
@@ -121,6 +140,8 @@ class NeuronPlacer:
         pass
 
     def place_neurons(self, neuron_type, number_of_neurons):
+
+        # använd np.choice ?
 
         pass
 
@@ -146,23 +167,10 @@ class NeuronBender:
 
 if __name__ == "__main__":
 
-    rmr = RegionMeshRedux(mesh_path="/home/hjorth/HBP/Snudda/snudda/data/mesh/Striatum-dorsal-right-hemisphere.obj")
+    mesh_path="/home/hjorth/HBP/Snudda/snudda/data/mesh/Striatum-dorsal-right-hemisphere.obj"
 
-    nep = NeuronPlacer(region_mesh=rmr, d_min=10e-6)
-    points = nep.get_point_cloud(n=1000000)
-    new_points = nep.remove_close_neurons(points)
-    new_inside_points = nep.remove_outside(new_points)
-
-    import matplotlib.pyplot as plt
-
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    x,y,z = new_inside_points.T
-    ax.scatter(x, y, z, marker='.')
-    plt.show()
-
-    # Lets calculate the distance to border
-    distance = nep.region_mesh.distance_to_border(new_inside_points)
+    nep = NeuronPlacer(mesh_path=mesh_path, d_min=10e-6)
+    nep.plot_putative_points()
 
     import pdb
     pdb.set_trace()