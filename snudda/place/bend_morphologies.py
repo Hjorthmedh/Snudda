a91f28f06f808b7ebf96766e235c7844a6854b52
Johannes Hjorth <hjorth@kth.se>
2023-10-12 11:49:33 +0200

    Work in progress

--- snudda/place/bend_morphologies.py
+++ snudda/place/bend_morphologies.py
@@ -22,6 +22,8 @@ class BendMorphologies:
     def bend_morphology(self, morphology: NeuronMorphologyExtended, k=50e-6):
 
         # k -- decay constant
+        n_random = 10
+        candidate_pos = np.zeros((n_random, 3))
 
         parent_rotation_matrices = dict()  # indexed by parent_point_idx in SectionMetaData
 
@@ -36,6 +38,8 @@ class BendMorphologies:
             else:
                 rotation_matrix = np.eye(3)
 
+            parent_coords = section.morphology_data.position[section.parent_section_idx, :]
+
             # Loop over all points in section
             for point_idx in section.point_idx:
                 coords = section.morphology_data.geometry[point_idx, :]
@@ -46,10 +50,33 @@ class BendMorphologies:
                 if self.rng.uniform(1) < P:
                     # We need to randomize new rotation matrix
                     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.transform.Rotation.html
-                    Rotation
-                    pass
+                    angles = self.rng.uniform(size=(n_random, 3), low=-0.2, high=0.2)  # Angles in radians
+                    rots = Rotation.from_euler(seq="XYZ", angles=angles)
+
+                    for idx, rot in enumerate(rots):
+                        candidate_pos[idx, :] = parent_coords[idx, :] + rots.apply(vectors=coords-parent_coords)
+
+                    candidate_dist = self.region_mesh.distance_to_border(points=candidate_pos)
+
+                    P_candidate = np.divide(1, 1 + np.exp(-k * candidate_dist))
+                    picked_idx = self.rng.choice(n_random, p=P_candidate)
+
+                    new_coords = candidate_pos[picked_idx, :]
+
+                    picked_rotation = rots[picked_idx]
+                    rotation_point = parent_coords
+
+                    # TODO: We also need to apply this rotation to ALL the other points in this section,
+                    #       and to all child sections of this branch.
+
+            else:
+                # Keep the old coords
+                new_coords = coords
 
 
+
+                # Old point is next points parent
+                parent_coords = new_coords
 
             # Gradient is calculated based on distance to mesh
             # Calculate delta_gradient = gradient_self - gradient_parent
@@ -63,4 +90,6 @@ class BendMorphologies:
 
 
 
-            pass
+    def rotate_subtree(self, section, from_idx, rotation, rotation_point):
+        
+        pass
