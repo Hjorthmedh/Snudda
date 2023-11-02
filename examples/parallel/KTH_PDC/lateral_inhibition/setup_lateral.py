6013544ffc5f1d95b50da4bfa15e282b0ce8bd01
Johannes Hjorth <hjorth@kth.se>
2023-11-02 16:01:21 +0100

    Updated script

---
+++ examples/parallel/KTH_PDC/lateral_inhibition/setup_lateral.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+network_path = "networks/lateral_1"
+# snudda_data = "$HOME/BasalGangliaData/data"
+snudda_data = "../../../../../BasalGangliaData/data/"
+
+duration=5
+
+from snudda import SnuddaInit
+
+n_DSPN = 30000
+n_ISPN = 30000
+n_FS = 0
+n_LTS = 0
+n_ChIN = 0
+
+si = SnuddaInit(network_path=network_path, snudda_data=snudda_data, random_seed=12345, honor_stay_inside=False)
+si.define_striatum(num_dSPN=n_DSPN, num_iSPN=n_ISPN, num_FS=n_FS, num_LTS=n_LTS, num_ChIN=n_ChIN,
+                   volume_type="cube")
+
+# The centre of the cube is [0.00475, 0.004, 0.00775]. num_neurons is optional
+si.add_population_unit_density(structure_name="Striatum", neuron_types=["dSPN", "iSPN"], 
+                               unit_centre=np.array([0.00475, 0.004, 0.00775]) -np.array([0,50e-6,150e-6]),
+                               probability_function="(d < 200e-6)*1", num_neurons=200)
+si.add_population_unit_density(structure_name="Striatum", neuron_types=["dSPN", "iSPN"], 
+                               unit_centre=np.array([0.00475, 0.004, 0.00775]) -np.array([150e-6,0,0]),
+                               probability_function="(d < 200e-6) * 1", num_neurons=200)
+ 
+si.write_json()
