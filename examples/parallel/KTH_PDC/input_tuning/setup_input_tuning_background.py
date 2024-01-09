3dd91d35ec81a3ae17e8d463c5f052583aec0539
Johannes Hjorth <hjorth@kth.se>
2024-01-09 14:27:43 +0100

    Updated

--- examples/parallel/KTH_PDC/input_tuning/setup_input_tuning_background.py
+++ examples/parallel/KTH_PDC/input_tuning/setup_input_tuning_background.py
@@ -36,7 +36,7 @@ input_tuning.setup_background_input(input_types=["cortical_background", "thalami
                                     input_density=["1.15*0.05/(1+exp(-(d-30e-6)/5e-6))", "0.05*exp(-d/200e-6)"],
                                     input_fraction=[0.5, 0.5],
                                     num_input_min=10, num_input_max=500,
-                                    input_frequency=[1, 1], input_duration=10)
+                                    input_frequency=[2, 2], input_duration=10)
 
 print("All done with setup_input_tuning.py")
 
