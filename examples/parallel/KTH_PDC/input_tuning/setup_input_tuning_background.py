baac580324f487ab9e35eb2b1ec4361c15c97d32
Johannes Hjorth <hjorth@kth.se>
2024-01-09 18:07:43 +0100

    Added ispn optimisation

--- examples/parallel/KTH_PDC/input_tuning/setup_input_tuning_background.py
+++ examples/parallel/KTH_PDC/input_tuning/setup_input_tuning_background.py
@@ -26,7 +26,7 @@ print("Constructor done, calling setup_network.")
 
 neurons_path = os.path.join("$DATA", "neurons", "striatum")
 input_tuning.setup_network(neurons_path=neurons_path, 
-                           num_replicas=21,
+                           num_replicas=41,
                            neuron_types=neuron_type)
 
 print("Calling setup_input")
@@ -35,7 +35,7 @@ print("Calling setup_input")
 input_tuning.setup_background_input(input_types=["cortical_background", "thalamic_background"],
                                     input_density=["1.15*0.05/(1+exp(-(d-30e-6)/5e-6))", "0.05*exp(-d/200e-6)"],
                                     input_fraction=[0.5, 0.5],
-                                    num_input_min=10, num_input_max=500,
+                                    num_input_min=10, num_input_max=1000,
                                     input_frequency=[2, 2], input_duration=10)
 
 print("All done with setup_input_tuning.py")
