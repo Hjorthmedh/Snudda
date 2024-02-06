Fixed some of the units tests

snudda/input/input.py
@@ -280,7 +280,7 @@ class SnuddaInput(object):
 
                     it_group = nid_group.create_group(input_type)
                     spike_set = it_group.create_dataset("spikes", data=spike_mat, compression="gzip", dtype=np.float32)
-                    spike_set.attrs["num_pikes"] = num_spikes
+                    spike_set.attrs["num_spikes"] = num_spikes
 
                     it_group.attrs["section_id"] = neuron_in["location"][1].astype(np.int16)
                     it_group.attrs["section_x"] = neuron_in["location"][2].astype(np.float16)
@@ -2040,7 +2040,7 @@ class SnuddaInput(object):
     ############################################################################
 
     # Normally specify synapse_density which then sets number of inputs
-    # ie leave nSpikeTrains as None. If nSpikeTrains is set, that will then
+    # ie leave nSpikeTrains as None. If num_spike_trains is set, that will then
     # scale synapse_density to get the requested number of inputs (approximately)
 
     # For virtual neurons nSpikeTrains must be set, as it defines their activity

snudda/simulate/save_network_recording.py
@@ -282,20 +282,20 @@ class SnuddaSaveNetworkRecordings:
             print(f"Writing network output to {self.output_file}")
             out_file = h5py.File(self.output_file, "w")
 
-            meta_data = out_file.create_group("metaData")
+            meta_data = out_file.create_group("meta_data")
 
-            meta_data.create_dataset("networkFile", data=self.network_data["networkFile"])
+            meta_data.create_dataset("network_file", data=self.network_data["network_file"])
             out_file.create_group("neurons")
 
             if self.network_data:
-                neuron_id = np.array([x["neuronID"] for x in self.network_data["neurons"]])
-                meta_data.create_dataset("ID", data=neuron_id)
+                neuron_id = np.array([x["neuron_id"] for x in self.network_data["neurons"]])
+                meta_data.create_dataset("id", data=neuron_id)
 
-                for name in ["name", "type", "morphology", "parameterKey", "morphologyKey", "modulationKey"]:
+                for name in ["name", "type", "morphology", "parameter_key", "morphology_key", "modulation_key"]:
                     self.write_string_meta_data(group=meta_data, name=name)
 
-                meta_data.create_dataset("populationUnit", data=self.network_data["populationUnit"], compression="gzip")
-                meta_data.create_dataset("position", data=self.network_data["neuronPositions"], compression="gzip")
+                meta_data.create_dataset("population_unit", data=self.network_data["population_unit"], compression="gzip")
+                meta_data.create_dataset("position", data=self.network_data["neuron_positions"], compression="gzip")
 
                 # TODO: Also write extraAxons info to file
 

snudda/utils/swap_to_degenerated_morphologies.py
@@ -553,7 +553,7 @@ class SwapToDegeneratedMorphologies:
                     # Same spikes as before
                     spike_set = input_group.create_dataset("spikes", data=old_input_data["spikes"][keep_idx2, :],
                                                            compression="gzip", dtype=np.float32)
-                    spike_set.attrs["num_pikes"] = old_input_data["spikes"].attrs["nSpikes"][keep_idx2].astype(np.int32)
+                    spike_set.attrs["num_spikes"] = old_input_data["spikes"].attrs["num_spikes"][keep_idx2].astype(np.int32)
 
                     # New locations for the remapped synapses
                     new_sec_id[idx_remap] = sec_id
@@ -582,7 +582,7 @@ class SwapToDegeneratedMorphologies:
                 else:
                     input_group.create_dataset("spikes", data=old_input_data["spikes"][keep_idx, :],
                                                compression="gzip", dtype=np.float32)
-                    input_group["spikes"].attrs["nSpikes"] = old_input_data["spikes"].attrs["nSpikes"][keep_idx]
+                    input_group["spikes"].attrs["num_spikes"] = old_input_data["spikes"].attrs["num_spikes"][keep_idx]
                     input_group.attrs["section_id"] = new_sec_id[keep_idx]
                     input_group.attrs["section_x"] = new_sec_x[keep_idx]
                     input_group.attrs["parameter_id"] = old_input_data.attrs["parameter_id"][keep_idx]