91d0266ff9ec487abb0738180aaf8a579cffdc65
Johannes Hjorth <hjorth@kth.se>
2024-10-22 15:44:26 +0200

    Updated verify dspn cortical notebook

--- examples/parallel/KTH_PDC/input_tuning/purge_bad.sh
+++ examples/parallel/KTH_PDC/input_tuning/purge_bad.sh
@@ -9,3 +9,9 @@ python purge_bad_parameters.py networks/input_tuning_ispn_cortical_signal_10Hz n
 
 python purge_bad_parameters.py networks/input_tuning_ispn_thalamic_signal_10Hz networks/input_tuning_ispn_thalamic_signal_10Hz/figures/_bad/ --snudda_data /home/hjorth/HBP/2024-10-03/BasalGangliaData/data/
 
+
+python purge_bad_parameters.py networks/input_tuning_fs_cortical_signal_10Hz networks/input_tuning_fs_cortical_signal_10Hz/figures/_bad/ --snudda_data /home/hjorth/HBP/2024-10-03/BasalGangliaData/data/
+
+
+python purge_bad_parameters.py networks/input_tuning_fs_thalamic_signal_10Hz networks/input_tuning_fs_thalamic_signal_10Hz/figures/_bad/ --snudda_data /home/hjorth/HBP/2024-10-03/BasalGangliaData/data/
+
