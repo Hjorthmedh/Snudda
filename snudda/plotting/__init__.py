93181793ace74bd45eafd9a6e14d3631547e461b
Johannes Hjorth <hjorth@kth.se>
2024-10-30 13:01:26 +0100

    Added new plotly plots for neuromodulation

--- snudda/plotting/__init__.py
+++ snudda/plotting/__init__.py
@@ -5,4 +5,5 @@ from snudda.plotting.plot_spike_raster_v2 import SnuddaPlotSpikeRaster2
 from snudda.plotting.plot_traces import PlotTraces
 from snudda.plotting.plot_density import PlotDensity
 from snudda.plotting.plot_period_experiment import PlotPeriodExperiment
+import snudda.plotting.plotly.trace
 import snudda.plotting.Blender
