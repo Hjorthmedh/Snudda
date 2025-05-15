a7584cf22ecfd68a4cbce900beb3ddf5a3da8291
Johannes Hjorth <hjorth@kth.se>
2025-05-15 08:39:03 +0200

    Added stack printout if snudda_data is not set, so we know where it happened

--- snudda/utils/snudda_path.py
+++ snudda/utils/snudda_path.py
@@ -82,7 +82,8 @@ def get_snudda_data(snudda_data=None, config_file=None, network_path=None, verbo
 
     if snudda_data is None:
         snudda_data = os.path.join(os.path.dirname(__file__), os.pardir, "data")
-        warnings.warn(f"SNUDDA_DATA not set, using {snudda_data}")
+        import traceback
+        warnings.warn(f"SNUDDA_DATA not set, using {snudda_data}\nStack:\n{traceback.format_stack()}\n")
 
     if not os.path.isdir(snudda_data):
         raise ValueError(f"SNUDDA_DATA = {snudda_data} DOES NOT EXIST")
