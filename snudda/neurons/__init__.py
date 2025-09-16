dd1461a323de17190ea7b667e0a0a5d45a1191ac
Johannes Hjorth <hjorth@kth.se>
2025-09-16 14:11:23 +0200

    No longer having NeuroModulation as an import in 'neuron'

--- snudda/neurons/__init__.py
+++ snudda/neurons/__init__.py
@@ -1,5 +1,6 @@
 from snudda.neurons.neuron_morphology import NeuronMorphology
 from snudda.neurons.neuron_morphology_extended import NeuronMorphologyExtended
 from snudda.neurons.neuron_model_extended import NeuronModel
-from snudda.neurons.neuron_modulation import NeuronModulation
 
+# This depends on NEURON -- so let's not have it as a mandatory import
+# from snudda.neurons.neuron_modulation import NeuronModulation
