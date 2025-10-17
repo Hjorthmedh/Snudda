d4d37a716edebd700f4b03a5915b1cc591981181
Johannes Hjorth <hjorth@kth.se>
2025-10-17 08:12:38 +0200

    Disabled new test, that is in development

--- tests/test_bend_morphologies.py
+++ tests/test_bend_morphologies.py
@@ -1,8 +1,36 @@
+import numpy as np
 import unittest
 
+from snudda.neurons import NeuronMorphologyExtended
+from snudda.place.bend_morphologies import BendMorphologies
 
-class MyTestCase(unittest.TestCase):
+
+class TestBendMorphologies(unittest.TestCase):
     def test_something(self):
+
+        morph_path = "validation/striatum/dspn/str-dspn-e150602_c1_D1-mWT-0728MSN01-v20190508/WT-0728MSN01-cor-rep-ax.swc"
+        mesh_path = "validation/mesh/Striatum-d-right.obj"
+        nm = NeuronMorphologyExtended(swc_filename=morph_path)
+
+        bm = BendMorphologies(mesh_path, rng=np.random.default_rng(1))
+
+        pos = np.array([0.006, 0.004, 0.00205])
+
+        before = nm.clone(position=pos, rotation=np.eye(3))
+        after = nm.clone(position=pos, rotation=np.eye(3))
+
+        new_rot_rep, _ = bm.bend_morphology(after.get_morphology())
+        new_coord = bm.apply_rotation(after.get_morphology(), new_rot_rep)
+        after.get_morphology().geometry[:, :3] = new_coord
+
+        change = np.sum(np.abs(before.get_morphology().geometry[:, :3] - after.get_morphology().geometry[:, :3]))
+        print(f"Change = {change}")
+
+        return
+
+        import pdb
+        pdb.set_trace()
+
         self.assertEqual(True, False)  # add assertion here
 
 
