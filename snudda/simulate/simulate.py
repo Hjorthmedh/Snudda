310cf442af999f626e833e9b45646e62aad9a7b4
Johannes Hjorth <hjorth@kth.se>
2025-10-24 13:48:42 +0200

    Garbage collection

--- snudda/simulate/simulate.py
+++ snudda/simulate/simulate.py
@@ -15,7 +15,6 @@
 ############################################################################
 
 import copy
-import gc
 import json
 import os
 import re
@@ -3071,7 +3070,11 @@ class SnuddaSimulate(object):
 
         self.sim = None
 
-        gc.collect()
+        try:
+            import gc
+            gc.collect()
+        except:
+            pass
 
 ############################################################################
 
