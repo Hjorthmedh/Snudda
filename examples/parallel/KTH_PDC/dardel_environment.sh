ad859f81da85c425c4001d271098273604f555dd
Johannes Hjorth <hjorth@kth.se>
2026-09-04 10:41:16 +0200

    Updated dardel env

--- examples/parallel/KTH_PDC/dardel_environment.sh
+++ examples/parallel/KTH_PDC/dardel_environment.sh
@@ -3,12 +3,15 @@
 # IF $NO_ENGINES is set to 1, then ipyparallel setup is skipped
 # IF $NO_SIM is set to 1, then NEURON modules are not compiled
 
-
-
-# Will use N_ENGINES if set by user, otherwise default to SLURM_NTASKS - 2
-N_ENGINES=${N_ENGINES:-$((SLURM_NTASKS-2))}
+N_ENGINES=${N_ENGINES:-${SLURM_NTASKS:-10}}
 # let N_ENGINES="100"
 
+
+# In case we run this interactively, then no SLURM_JOB_NUM_NODES
+# Set SLURM_JOB_NUM_NODES to 1 if it is unset, empty, or <= 0
+if [ "${SLURM_JOB_NUM_NODES:-0}" -le 0 ]; then
+    SLURM_JOB_NUM_NODES=1
+fi
 
 # Make sure this is right python version
 export SNUDDA_DIR=$HOME/Snudda
@@ -31,61 +34,46 @@ module load neuron
 
 source $HOME/Snudda/snudda_env/bin/activate
 
-if [ -z "$NO_ENGINES" ]; then
+# We no longer need to start the engines, that is done by snudda itself
 
-    # IPYTHONDIR defaults to current folder, if network_path is not set
-    export IPYTHONDIR="/cfs/klemming/scratch/${USER:0:1}/$USER/.ipython-${SLURM_JOB_ID}"
-    export IPYTHON_PROFILE=default
-    
-    # Start the ipcontroller
-    export FI_CXI_DEFAULT_VNI=$(od -vAn -N4 -tu < /dev/urandom)
-    srun -n 1 -N 1 -c 2 --exact --overlap --mem=0 ./ipcontroller_new.sh &
+# Only compile NEURON MODules if user sets COMPILE_MOD=1
 
-    echo ">>> waiting 60s for controller to start"
-    sleep 60 
+# We set these, since RxD might ask to compile...
+export CC=cc
+export CXX=CC
+export FC=ftn
+export MPICC=cc
+export MPICXX=CC
 
-    #.. Read in CONTROLLERIP
-    CONTROLLERIP=$(<controller_ip.txt)
+if [  -n "$COMPILE_MOD"  ]; then
 
-    echo ">>> starting ${N_ENGINES} engines "
+    # Only recompile if the MOD files are missing
+    if [ ! -d "x86_64" ]; then
 
-    export FI_CXI_DEFAULT_VNI=$(od -vAn -N4 -tu < /dev/urandom)
-    srun -n ${N_ENGINES} -c 2 -N ${SLURM_JOB_NUM_NODES} --exact --overlap --mem=0 ipengine \
-	 --location=${CONTROLLERIP} --profile=${IPYTHON_PROFILE} --mpi \
-	 --ipython-dir=${IPYTHONDIR}  --timeout=30.0 c.EngineFactory.max_heartbeat_misses=10  c.MPI.use='mpi4py' \
-	 1> ipe_${SLURM_JOBID}.out 2> ipe_${SLURM_JOBID}.err &
+      SPECIAL_PATH=x86_64/special
 
+      # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CRAY_LD_LIBRARY_PATH
 
-    echo ">>> waiting 60s for engines to start"
-    sleep 30
+      # Explicitly adding MPI paths (suggested by ChatGTP, why are they not loaded automatically?)
+      # export LD_LIBRARY_PATH=/opt/cray/pe/mpich/8.1.31/ofi/gnu/12.3/lib:$LD_LIBRARY_PATH
+      # export MPI_LIB_NRN_PATH=/opt/cray/pe/mpich/8.1.31/ofi/gnu/12.3/lib
 
-fi
-    
+      # Clear old compilation
+      rm -rf x86_64 2>/dev/null || true
 
-if [ -z "$NO_SIM" ]; then
+      export CXX=CC
+      export CC=cc
+      export FC=ftn
+      export MPICC=cc
+      export MPICXX=CC
 
-    SPECIAL_PATH=x86_64/special
+      CC --version
 
-    # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CRAY_LD_LIBRARY_PATH
+      export FI_CXI_DEFAULT_VNI=$(od -vAn -N4 -tu < /dev/urandom)
+      # srun -n 1 nrnivmodl -incflags "-lltdl=/usr/lib64/libltdl.so.7 -lreadline=/lib64/libreadline.so.7 -lncurses=/lib64/libncurses.so.6.1" -loadflags "-DLTDL_LIBRARY=/usr/lib64/libltdl.so.7 -DREADLINE_LIBRARY=/lib64/libreadline.so.7 -DNCURSES_LIBRARY=/lib64/libncurses.so.6.1" mechanisms/
 
-    # Explicitly adding MPI paths (suggested by ChatGTP, why are they not loaded automatically?)
-    # export LD_LIBRARY_PATH=/opt/cray/pe/mpich/8.1.31/ofi/gnu/12.3/lib:$LD_LIBRARY_PATH
-    # export MPI_LIB_NRN_PATH=/opt/cray/pe/mpich/8.1.31/ofi/gnu/12.3/lib
+      srun -n 1 nrnivmodl mechanisms/
 
-    # Clear old compilation
-    rm -rf x86_64 2>/dev/null || true
+    fi
 
-    export CXX=CC
-    export CC=cc
-    export FC=ftn
-    export MPICC=cc
-    export MPICXX=CC
-
-    CC --version
-
-    export FI_CXI_DEFAULT_VNI=$(od -vAn -N4 -tu < /dev/urandom)
-    # srun -n 1 nrnivmodl -incflags "-lltdl=/usr/lib64/libltdl.so.7 -lreadline=/lib64/libreadline.so.7 -lncurses=/lib64/libncurses.so.6.1" -loadflags "-DLTDL_LIBRARY=/usr/lib64/libltdl.so.7 -DREADLINE_LIBRARY=/lib64/libreadline.so.7 -DNCURSES_LIBRARY=/lib64/libncurses.so.6.1" mechanisms/
-
-    srun -n 1 nrnivmodl mechanisms/
-    
 fi
