dadf9a7c434318fca5b1f9b18933dc6f45d8c6e9
Johannes Hjorth <hjorth@kth.se>
2026-09-08 14:56:04 +0200

    Update

--- snudda/simulate/network_pair_pulse_simulation.py
+++ snudda/simulate/network_pair_pulse_simulation.py
@@ -149,8 +149,7 @@ class SnuddaNetworkPairPulseSimulation:
               slice_depth = 150e-6,
               slice_side_len = 500e-6,
               random_seed=None, density=80500, d_min=15e-6,
-              parallel=False,
-              ipython_profile=None):
+              parallel=False):
 
         if random_seed:
             self.random_seed = random_seed
@@ -165,7 +164,7 @@ class SnuddaNetworkPairPulseSimulation:
             slice_depth = None
 
         from snudda import Snudda
-        snd = Snudda(network_path=self.network_path, parallel=parallel, ipython_profile=ipython_profile)
+        snd = Snudda(network_path=self.network_path, parallel=parallel)
 
         snd.init_tiny(neuron_paths=neuron_paths,
                       neuron_names=neuron_names,
